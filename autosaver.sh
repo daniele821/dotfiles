6ad7ce3dce9ec1a3733a33af86d255467028baf5
Daniele Muffato <danixgithub1@gmail.com>
2024-01-05 00:08:35 +0100

    added some util functions

--- autosaver.sh
+++ autosaver.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# autosaver.sh                  <Daniele Muffato>
+#
+# script to track and backup files in the home directory
+# and to save init scripts to run at OS installation
+#
 # WARNING: this file MUST be in the root of the dotfiles directory
 
-### VARIABLES 
-# WARNING: array elements MUST not be changed of position
+### VARIABLES
 SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 SCRIPT_NAME="$(basename "${SCRIPT_PWD}")"
@@ -23,6 +27,28 @@ CONFIG_FILES=(
     "${DIRS[1]}/init-scripts.txt"
 )
 
-### UTILITY FUNCTIONS
+### COLOR FUNCTIONS
+function clr_file(){
+    echo -e "\e[1;36m${1}\e[m\c"
+}
+function clr_warn(){
+    echo -e "\e[1;33mWARNING:\e[m ${1}"
+}
+function clr_err(){
+    echo -e "\e[1;31mERROR:\e[m ${1}"
+}
+function clr_err_quit(){
+    clr_err "${1}"
+    exit 1
+}
 
-### ACTUAL EXECUTION
+### GIT FUNCTIONS
+function git_is_repo_empty(){
+    GIT_OBJECTS="$(git -C "${SCRIPT_DIR}" count-objects 2>/dev/null | awk '{print $1}')"
+    [[ "${GIT_OBJECTS}" -gt "0" ]]
+}
+function git_current_branch(){
+    git -C "${SCRIPT_DIR}" rev-parse --abbrev-ref HEAD 
+}
+
+
