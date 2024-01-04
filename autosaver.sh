086b521432dc6301e7b8b684b8b09fedd78ecbb6
Daniele Muffato <danixgithub1@gmail.com>
2024-01-05 00:35:57 +0100

    added check and fix git user name/email

--- autosaver.sh
+++ autosaver.sh
@@ -35,13 +35,13 @@ function clr_file(){
     echo -e "\e[1;36m${1}\e[m\c"
 }
 function clr_warn(){
-    echo -e "\e[1;33mWARNING:\e[m ${1}"
+    echo -e "\e[1;33mWARNING:\e[m ${1}\c"
 }
 function clr_err(){
-    echo -e "\e[1;31mERROR:\e[m ${1}"
+    echo -e "\e[1;31mERROR:\e[m ${1}\c"
 }
 function clr_err_quit(){
-    clr_err "${1}"
+    clr_err "${1}\n"
     exit 1
 }
 
@@ -50,11 +50,24 @@ function git_check_branch_quit(){
     # check and exits if git repo is empty (ie: no commits)
     GIT_OBJECTS="$(git -C "${SCRIPT_DIR}" count-objects 2>/dev/null | awk '{print $1}')"
     [[ "${GIT_OBJECTS}" -gt "0" ]] || clr_err_quit "this git repo is empty!"
-
     # checks and exit if current branch is not whitelisted
     CURRENT="$(git -C "${SCRIPT_DIR}" rev-parse --abbrev-ref HEAD)"
     WHITELISTED="$(cat "${USER_CONFIG_FILES[0]}" 2>/dev/null)"
     [[ "${CURRENT}" != "${WHITELISTED}" ]] && clr_err_quit "the current branch is not whitelisted!"
+}
+function git_fix_user(){
+    # fix user.name
+    while [[ -z "$(git -C "${SCRIPT_DIR}" config user.name)" ]]; do
+        clr_warn "git user name is invalid! Insert git name: "
+        read -r answer
+        git -C "${SCRIPT_DIR}" config user.name "${answer}"
+    done
+    # fix user.email
+    while [[ -z "$(git -C "${SCRIPT_DIR}" config user.email)" ]]; do
+        clr_warn "git user email is invalid! Insert git email: "
+        read -r answer
+        git -C "${SCRIPT_DIR}" config user.email "${answer}"
+    done
 }
 
 
