9151b0c147d2083656446569b94785758d49aaf1
Daniele Muffato <danixgithub1@gmail.com>
2024-02-02 15:04:47 +0100

    removed rofi and added init scripts

---
+++ init/01_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+echo -e "\e[1;33mWARNING:\e[m automatic installation is unstable (thanks packagekitd). Do installation manually, using these init script as a base\e[m" && exit 1
+
+sudo zypper install --no-confirm hyprland waybar ripgrep kitty neovim vim git lsd starship NetworkManager-applet blueman bat nodejs-common zoxide fzf ShellCheck libnotify
+sudo zypper remove --no-confirm -u gnome-software gnome-shell-classic
---
+++ init/02_localops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
+
+"${SCRIPT_DIR}"/autosaver.sh restore
+
+sudo mkdir /personal
+sudo chown "$USER":"$USER" /personal
+mkdir /personal/repos
+
+cd ~/.local/share/fonts/FiraCode && unzip FiraCode.zip
