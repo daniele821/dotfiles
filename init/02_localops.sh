1b8f785a5106542f6349d04e4bc90e04018ac4f0
Daniele Muffato <danixgithub1@gmail.com>
2024-02-04 01:12:51 +0100

    added Dracula theme

--- init/02_localops.sh
+++ init/02_localops.sh
@@ -12,3 +12,4 @@ sudo chown "$USER":"$USER" /personal
 mkdir /personal/repos
 
 cd ~/.local/share/fonts/FiraCode && unzip FiraCode.zip
+cd ~/.local/share/themes/ && unzip gtk-master.zip && mv gtk-master Dracula
