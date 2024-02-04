94176b095134acc8e2ee8fbd0ff42461824f99e4
Daniele Muffato <danixgithub1@gmail.com>
2024-02-04 23:24:51 +0100

    n

--- init/01_packages.sh
+++ init/01_packages.sh
@@ -5,3 +5,4 @@ set -e
 sudo zypper install --no-confirm hyprland waybar ripgrep kitty neovim vim git lsd starship NetworkManager-applet blueman bat nodejs-common zoxide fzf ShellCheck libnotify gcc
 sudo zypper remove --no-confirm -u gnome-software gnome-shell-classic
 sudo zypper remove --no-confirm sway icewm
+sudo zypper addlock gnome-software gnome-shell-classic sway icewm
