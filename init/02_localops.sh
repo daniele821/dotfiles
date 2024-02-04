e182a16cafbf2535203d77e8be3d649bd11ef092
Daniele Muffato <danixgithub1@gmail.com>
2024-02-04 03:52:37 +0100

    added gsettings command to set gtk theme

--- init/02_localops.sh
+++ init/02_localops.sh
@@ -13,3 +13,8 @@ mkdir /personal/repos
 
 cd ~/.local/share/fonts/FiraCode && unzip FiraCode.zip
 cd ~/.local/share/themes/ && unzip gtk-master.zip && mv gtk-master Dracula
+
+# this and GTK_THEME=Dracula (already defined in hyprland conf file) kinda work in theming everything
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+
