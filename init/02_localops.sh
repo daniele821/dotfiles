bd41a254a4d5b1eb8e9e5fdb2499f9f0de736053
Daniele Muffato <danixgithub1@gmail.com>
2024-02-06 18:29:33 +0100

    small fix to init/02.sh

--- init/02_localops.sh
+++ init/02_localops.sh
@@ -3,7 +3,7 @@
 SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 
-"${SCRIPT_DIR}"/autosaver.sh restore
+"${SCRIPT_DIR}"/../autosaver.sh restore
 
 sudo mkdir /personal
 sudo chown "$USER":"$USER" /personal
