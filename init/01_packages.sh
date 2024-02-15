13848618b3044613506f2f62ab82e97eeb52e576
Daniele Muffato <danixgithub1@gmail.com>
2024-02-15 11:33:39 +0100

    removed hyprpaper

--- init/01_packages.sh
+++ init/01_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo zypper install --no-confirm hyprland hyprpaper waybar ripgrep kitty neovim vim git lsd starship NetworkManager-applet bat nodejs-common zoxide fzf ShellCheck libnotify gcc libqt5-qtwayland qt6-wayland
+sudo zypper install --no-confirm hyprland waybar ripgrep kitty neovim vim git lsd starship NetworkManager-applet bat nodejs-common zoxide fzf ShellCheck libnotify gcc libqt5-qtwayland qt6-wayland
 sudo zypper remove --no-confirm -u gnome-software gnome-shell-classic opensuse-welcome
 sudo zypper remove --no-confirm sway icewm
 sudo zypper addlock gnome-software gnome-shell-classic sway icewm opensuse-welcome
