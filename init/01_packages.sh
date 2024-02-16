b006c7509fafd27a08d08e261a79350a3a244613
Daniele Muffato <danixgithub1@gmail.com>
2024-02-16 14:21:45 +0100

    auto install of packman repo and obs (for media codecs, otherwise videos suck)

--- init/01_packages.sh
+++ init/01_packages.sh
@@ -4,3 +4,8 @@ sudo zypper install --no-confirm hyprland waybar ripgrep kitty neovim vim git ls
 sudo zypper remove --no-confirm -u gnome-software gnome-shell-classic opensuse-welcome
 sudo zypper remove --no-confirm sway icewm
 sudo zypper addlock gnome-software gnome-shell-classic sway icewm opensuse-welcome
+
+# add packam repo for multimedia codecs and stuff like obs
+sudo zypper addrepo -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/' packman
+sudo zypper dist-upgrade --no-confirm --from packman --allow-vendor-change
+sudo zypper install --no-confirm obs-studio
