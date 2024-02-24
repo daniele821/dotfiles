f59946b3d5cd7e33cc40466dfbde97c3a3919e6b
Daniele Muffato <danixgithub1@gmail.com>
2024-02-24 17:55:40 +0100

    small updates

--- init/01_packages.sh
+++ init/01_packages.sh
@@ -5,7 +5,7 @@ sudo zypper remove --no-confirm -u gnome-software gnome-shell-classic opensuse-w
 sudo zypper remove --no-confirm sway icewm
 sudo zypper addlock gnome-software gnome-shell-classic sway icewm opensuse-welcome
 
-# add packam repo for multimedia codecs and stuff like obs
+# add packam repo for multimedia codecs
 sudo zypper addrepo -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/' packman
 sudo zypper dist-upgrade --from packman --allow-vendor-change
-sudo zypper install obs-studio
+sudo zypper install --from packman ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full vlc-codecs
