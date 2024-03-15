985d55f4cc0844daf774b3c98f76b03f73ace20e
Daniele Muffato <danixgithub1@gmail.com>
2024-03-15 20:39:20 +0100

    added functions to open on specific workspaces

--- backup/.bashrc.d/functions.sh
+++ backup/.bashrc.d/functions.sh
@@ -3,9 +3,18 @@
 function __exec_nohupped__(){
     (:; nohup "${@}" &>/dev/null &)
 }
+function __hypr_workspace__(){
+    hyprctl dispatch workspace "${@}" &>/dev/null
+}
 function open(){
     for file in "${@}"; do __exec_nohupped__ "xdg-open" "${file}"; done
 }
 function run(){
     __exec_nohupped__ "${@}" 
+}
+function openhypr(){
+    __hypr_workspace__ "${1}" && open "${@:2}"
+}
+function runhypr(){
+    __hypr_workspace__ "${1}" && run "${@:2}"
 }
