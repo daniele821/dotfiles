90b2620d951c8b2a98e8497c1483dc9b9b85a453
Daniele Muffato <danixgithub1@gmail.com>
2024-04-01 20:34:20 +0200

    added download of multimedia codecs from rpm fusion repos

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -2,14 +2,21 @@
 
 {
 ### upgrade everything ###
-sudo dnf upgrade
+sudo dnf --assumeyes upgrade
 
 # basic stuff
-sudo dnf remove gnome-classic-session gnome-boxes gnome-software cheese gnome-tour gnome-maps yelp rhythmbox simple-scan 
-sudo dnf install zoxide bat ripgrep gnome-tweaks lsd neovim gcc
+sudo dnf --assumeyes remove gnome-classic-session gnome-boxes gnome-software cheese gnome-tour gnome-maps yelp rhythmbox simple-scan 
+sudo dnf --assumeyes install zoxide bat ripgrep gnome-tweaks lsd neovim gcc
 curl -sS https://starship.rs/install.sh | sh
 
+# enable rpm-fusion and install multimedia codecs
+sudo dnf --assumeyes install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-39.noarch.rpm 
+sudo dnf --assumeyes install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-39.noarch.rpm  
+sudo dnf --assumeyes install gstreamer1-plugins-bad-* gstreamer1-plugins-good-* gstreamer1-plugins-base gstreamer1-plugin-openh264 gstreamer1-plugin-libav --exclude=gstreamer1-plugins-bad-free-devel  
+sudo dnf --assumeyes install lame* --exclude=lame-devel  
+sudo dnf --assumeyes group upgrade --with-optional --allowerasing Multimedia # WARNING: avoid running twice, as it pollutes dnf history
+
 ### remove unnecessary packages ###
-sudo dnf autoremove
+sudo dnf --assumeyes autoremove
 
 } < /dev/tty
