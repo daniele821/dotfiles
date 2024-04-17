7711c6a3e42e4b9258e27b3844db8be976d0f1e3
Daniele Muffato <danixgithub1@gmail.com>
2024-04-17 16:59:53 +0200

    an other one small fix to init script 04

--- init/04_optionals.sh
+++ init/04_optionals.sh
@@ -15,15 +15,18 @@ fi
 
 # install dbmain 
 if  ! [[ -d '/personal/exe/dbmain' ]] && ask_user 'Do you want to install dbmain [warning: potentially dangerous]' ; then
-    TMP_FILE="$(mktemp)"
-    FILE=dbmain.tar.gz
-    curl -Z 'https://projects.info.unamur.be/dbmain/files/dbm-1102-linux-amd64-setup.tar.gz' > "${TMP_FILE}" || exit 1
-    mkdir -p /personal/exe/dbmain/ 
-    cd /personal/exe/dbmain || exit 1
-    mv "${TMP_FILE}" "${FILE}"
-    tar -xzf "${FILE}"
-# shellcheck disable=SC2016
-    echo '
+    if ! [[ -d '/personal/exe' ]]; then
+        echo 'you need to create /personal/exe directory to install dbmain'
+    else
+        TMP_FILE="$(mktemp)"
+        FILE=dbmain.tar.gz
+        curl -Z 'https://projects.info.unamur.be/dbmain/files/dbm-1102-linux-amd64-setup.tar.gz' > "${TMP_FILE}" || exit 1
+        mkdir -p /personal/exe/dbmain/ 
+        cd /personal/exe/dbmain || exit 1
+        mv "${TMP_FILE}" "${FILE}"
+        tar -xzf "${FILE}"
+        # shellcheck disable=SC2016
+        echo '
 # dbmain
 function dbm() {
 	export DB_MAIN_BIN=/personal/exe/dbmain/bin
@@ -33,19 +36,23 @@ function dbm() {
 function db_main() {
 	dbm && "${DB_MAIN_BIN}"/db_main "${@}"
 }' >> "$HOME/.bashrc"
-    rm "${FILE}"
+        rm "${FILE}"
+    fi
 fi
 
 if ! [[ -d '/personal/exe/anaconda' ]] && ask_user 'Do you want to install anaconda [warning: potentially dangerous]'; then
-    # ANACONDA FOR SOME FUCKING REASON CHECKS THE FUCKING FILE EXTENSION. WHO WROTE THIS SHIT?
-    TMP_FILE="$(mktemp /tmp/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.sh)"
-    curl -Z 'https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh' > "${TMP_FILE}" || exit 1
-    chmod +x "${TMP_FILE}"
-    echo -e 'WARNING: install anaconda in /personal/exe/anaconda or auto install will break;\nPress enter to continue...'
-    read -r _ </dev/tty
-    "${TMP_FILE}" </dev/tty
-# shellcheck disable=SC2016
-    echo '
+    if ! [[ -d '/personal/exe' ]]; then
+        echo 'you need to create /personal/exe directory to install anaconda'
+    else
+        # ANACONDA FOR SOME FUCKING REASON CHECKS THE FUCKING FILE EXTENSION. WHO WROTE THIS SHIT?
+        TMP_FILE="$(mktemp /tmp/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.sh)"
+        curl -Z 'https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh' > "${TMP_FILE}" || exit 1
+        chmod +x "${TMP_FILE}"
+        echo -e 'WARNING: install anaconda in /personal/exe/anaconda or auto install will break;\nPress enter to continue...'
+        read -r _ </dev/tty
+        "${TMP_FILE}" </dev/tty
+        # shellcheck disable=SC2016
+        echo '
 # anaconda
 function epe() {
 	export ANACONDA_BIN=/personal/exe/anaconda/bin
@@ -55,5 +62,6 @@ function epe() {
 function jupyter-lab() {
 	epe && "${ANACONDA_BIN}"/jupyter-lab
 } ' >> "$HOME/.bashrc"
-    rm "${TMP_FILE}" 
+        rm "${TMP_FILE}" 
+    fi
 fi
