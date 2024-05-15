65b02fb25f05ea72a3ed348b395d9429b3c9501f
Daniele Muffato <danixgithub1@gmail.com>
2024-05-15 11:30:55 +0200

    reorganized autoinstalled packages by reason to install

--- init/01_packages.sh
+++ init/01_packages.sh
@@ -4,10 +4,14 @@
 ### upgrade everything ###
 sudo dnf --assumeyes upgrade
 
-# basic stuff
-sudo dnf --assumeyes install zoxide bat ripgrep lsd neovim gcc libreoffice-langpack-it
+# install neovim and goodies
+sudo dnf --assumeyes install neovim gcc fd-find ripgrep 
+# install some nice terminal cli tools
+sudo dnf --assumeyes install zoxide bat lsd 
 sudo dnf --assumeyes copr enable atim/starship
 sudo dnf --assumeyes install starship
+# other
+sudo dnf --assumeyes install libreoffice-langpack-it 
 
 ### remove unnecessary packages ###
 sudo dnf --assumeyes autoremove
