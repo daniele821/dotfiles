0730bcfac9490d7e71157f30984d3c8371b31437
Daniele Muffato <danixgithub1@gmail.com>
2024-05-15 12:26:27 +0200

    readded faster dnf init script file

---
+++ init/01_fasterdnf.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+cat /etc/dnf/dnf.conf
+echo -en "\nDo you want to append the two lines to speed up dnf [y/n]? "
+read -r answer </dev/tty
+[[ "${answer,,:0:1}" != "y" ]] && exit 0
+echo 'max_parallel_downloads=10
+fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf
+
--- init/02_packages.sh
+++ init/02_packages.sh
@@ -6,10 +6,12 @@ sudo dnf --assumeyes upgrade
 
 # install neovim and goodies
 sudo dnf --assumeyes install neovim gcc fd-find ripgrep 
+
 # install some nice terminal cli tools
 sudo dnf --assumeyes install zoxide bat lsd 
 sudo dnf --assumeyes copr enable atim/starship
 sudo dnf --assumeyes install starship
+
 # other
 sudo dnf --assumeyes install libreoffice-langpack-it 
 
