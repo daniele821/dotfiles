2f3f8e9dd32b3703ce1afb406261fc0f78ce299f
Daniele Muffato <danixgithub1@gmail.com>
2024-05-30 21:59:03 +0200

    autoremove konsole toolbars (such hacky)

--- init/04_kdeconfig.sh
+++ init/04_kdeconfig.sh
@@ -19,7 +19,11 @@ kwriteconfig6 --file ~/.config/okularpartrc --group 'General' --key 'ShellOpenFi
 kwriteconfig6 --file ~/.config/konsolerc --group 'KonsoleWindow' --key 'RememberWindowSize' --type bool false
 kwriteconfig6 --file ~/.config/konsolerc --group 'Notification Messages' --key 'CloseAllTabs' --type bool true
 kwriteconfig6 --file ~/.config/konsolerc --group 'Notification Messages' --key 'CloseAllEmptyTabs' --type bool true
-kwriteconfig6 --file ~/.config/konsolerc --group 'Desktop Entry' --key 'DefaultProfile' 'Nerd Font.profile' # requires profile file
+if [[ -f "${HOME}/.local/share/konsole/Nerd Font.profile" ]]; then
+	kwriteconfig6 --file ~/.config/konsolerc --group 'Desktop Entry' --key 'DefaultProfile' 'Nerd Font.profile'
+fi
+### HACKY: magically removes toolbars
+kwriteconfig6 --file ~/.local/share/konsole/konsolestaterc --group 'MainWindow' --key 'State' 'AAAA/wAAAAD9AAAAAQAAAAAAAAAAAAAAAPwCAAAAAvsAAAAiAFEAdQBpAGMAawBDAG8AbQBtAGEAbgBkAHMARABvAGMAawAAAAAA/////wAAAXIA////+wAAABwAUwBTAEgATQBhAG4AYQBnAGUAcgBEAG8AYwBrAAAAAAD/////AAABEQD///8AAASyAAAC7AAAAAQAAAAEAAAACAAAAAj8AAAAAQAAAAIAAAACAAAAFgBtAGEAaQBuAFQAbwBvAGwAQgBhAHIAAAAAAP////8AAAAAAAAAAAAAABwAcwBlAHMAcwBpAG8AbgBUAG8AbwBsAGIAYQByAAAAAAD/////AAAAAAAAAAA='
 
 # configure krunner
 kwriteconfig6 --file ~/.config/krunnerrc --group 'Plugins' --key 'krunner_appstreamEnabled' --type bool false
