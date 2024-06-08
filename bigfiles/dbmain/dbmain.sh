187d8e7b36c109146b2b788f84730fceb2f8d4f9
Daniele Muffato <danixgithub1@gmail.com>
2024-06-08 22:44:43 +0200

    removed dbmain

--- bigfiles/dbmain/dbmain.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
-SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
-export DB_MAIN_BIN="${SCRIPT_DIR}/bin"
-export PATH=$DB_MAIN_BIN:$PATH
-export LD_LIBRARY_PATH=$DB_MAIN_BIN:$DB_MAIN_BIN/../java/jre/lib/amd64/server:$LD_LIBRARY_PATH
-db_main "${@}"
