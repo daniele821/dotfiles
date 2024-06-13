82d34f18cd27d185e207ce1b14e547d70995a308
Daniele Muffato <danixgithub1@gmail.com>
2024-06-13 15:47:27 +0200

    small change

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -2,13 +2,10 @@
 
 {
 	# install utilities
-	sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr libreoffice-langpack-it hyprland wlsunset waybar swaylock wireshark
+	sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr libreoffice-langpack-it wireshark hyprland wlsunset waybar swaylock brightnessctl
 
 	# uninstall bloat
 	sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool kwalletmanager krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel kitty
-
-	# reinstall utilities removed while uninstalling bloat
-	sudo dnf --assumeyes install brightnessctl
 
 	# upgrade everything #
 	sudo dnf --assumeyes upgrade
