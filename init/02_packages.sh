d6efc5002332c3f964951225b5c43435e1bb9cb9
Daniele Muffato <danixgithub1@gmail.com>
2024-06-14 19:05:32 +0200

    added mpv autoinstall (haruna has more feauters, and better theme, but cannot hide status bar)

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
 {
-	# install utilities
-	sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr libreoffice-langpack-it wireshark hyprland wlsunset waybar swaylock brightnessctl
+	# install utilities (mpv or haruna for media playing)
+	sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr libreoffice-langpack-it wireshark mpv hyprland wlsunset waybar swaylock brightnessctl
 
 	# uninstall bloat
 	sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool kwalletmanager krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel kitty
