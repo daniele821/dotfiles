d27648d018dd94524d57f4b39b7f1becfa2a2733
Daniele Muffato <danixgithub1@gmail.com>
2024-06-15 23:33:50 +0200

    autoremove libreport (bloat)

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -5,7 +5,7 @@
 	sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr libreoffice-langpack-it wireshark mpv hyprland wlsunset waybar swaylock brightnessctl
 
 	# uninstall bloat
-	sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool kwalletmanager krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel kitty
+	sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool kwalletmanager krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel libreport kitty
 
 	# enable rpmfusion repos
 	sudo dnf --assumeyes install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-40.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-40.noarch.rpm
