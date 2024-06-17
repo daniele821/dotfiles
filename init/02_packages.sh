b65a4d675a00c1d6e3b0294390d115347dc246ef
Daniele Muffato <danixgithub1@gmail.com>
2024-06-17 20:00:27 +0200

    don't install rpmfusion and ffmpeg, since bluetooth still stutters

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -7,12 +7,6 @@
 	# uninstall bloat
 	sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool kwalletmanager krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel libreport kitty
 
-	# enable rpmfusion repos
-	sudo dnf --assumeyes install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-40.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-40.noarch.rpm
-
-	# install ffmpeg (fixes bluetooth stuttering)
-	sudo dnf --assumeyes --allowerasing install ffmpeg
-
 	# upgrade everything #
 	sudo dnf --assumeyes upgrade
 
