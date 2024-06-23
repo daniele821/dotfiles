a811fd103f273b69b93d2e61f586e4b91e9bfb7b
Daniele Muffato <danixgithub1@gmail.com>
2024-06-23 15:11:23 +0200

    changed how switching tiling/float window work

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -2,7 +2,7 @@
 
 {
 	# install utilities (mpv or haruna for media playing)
-	sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr libreoffice-langpack-it wireshark mpv libsixel-utils hyprland gammastep waybar swaylock brightnessctl
+	sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr libreoffice-langpack-it wireshark mpv libsixel-utils jq hyprland gammastep waybar swaylock brightnessctl
 
 	# uninstall bloat
 	sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool kwalletmanager krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel libreport kitty
