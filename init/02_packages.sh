4e4301ca2d0568787f55e32f70e367fbc42e0ad4
Daniele Muffato <danixgithub1@gmail.com>
2024-07-07 21:33:27 +0200

    autoinstall htop

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -5,7 +5,7 @@
     sudo dnf --assumeyes copr enable atim/starship
 
     # install utilities (mpv or haruna for media playing)
-    sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr libreoffice-langpack-it wireshark mpv libsixel-utils starship jq hyprland gammastep waybar swaylock brightnessctl
+    sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr starship htop libreoffice-langpack-it wireshark mpv libsixel-utils jq hyprland gammastep waybar swaylock brightnessctl
 
     # uninstall bloat
     sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool kwalletmanager krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel libreport kitty
