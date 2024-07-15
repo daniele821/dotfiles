ca3266792865580dee82563583fda4412f1731e6
Daniele Muffato <danixgithub1@gmail.com>
2024-07-15 19:29:24 +0200

    added function to preview images one per time

--- backup/.bashrc.d/init/img2sixel.sh
+++ backup/.bashrc.d/init/img2sixel.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-function preview() {
+function preview-all() {
     COLS="$(tput cols)"
     ((COLS = COLS * 5))
     for elem in "${@}"; do
         echo "${elem}"
         img2sixel -w $COLS "${elem}"
     done | less -r
+}
+
+function preview() {
+    for file in "${@}"; do
+        preview-all "${file}"
+    done
 }
