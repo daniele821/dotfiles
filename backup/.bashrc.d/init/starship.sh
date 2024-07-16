47b89be9507ace4c51a617789e0d82206a8e0f97
Daniele Muffato <danixgithub1@gmail.com>
2024-07-16 17:27:23 +0200

    brought back starship hacky fixes

--- backup/.bashrc.d/init/starship.sh
+++ backup/.bashrc.d/init/starship.sh
@@ -1,4 +1,11 @@
 #!/bin/env bash
 
+## various fixes ##
+function blastoff() {
+    git status -s &>/dev/null
+}
+export starship_precmd_user_func="blastoff"
+export STARSHIP_LOG=error
+
 ## startup program ##
 eval "$(starship init bash)"
