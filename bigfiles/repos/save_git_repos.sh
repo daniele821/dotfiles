5bd514af6fadcb809bf0ef33e999b20ba4b9a45e
Daniele Muffato <danixgithub1@gmail.com>
2024-07-21 12:21:38 +0200

    improved script to save git repois

--- bigfiles/repos/save_git_repos.sh
+++ bigfiles/repos/save_git_repos.sh
@@ -5,7 +5,10 @@ SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 BACKUP_FILE="${SCRIPT_DIR}/git_repos.txt"
 
 find /personal/repos/ -iname .git | while read -r dir; do
-    echo -ne "$(dirname "$dir")\t --> \t"
-    grep url "$dir"/config | awk '{printf $3}'
-    echo
+    DIR="$(dirname "${dir}")"
+    if git -C "${DIR}" remote get-url origin &>/dev/null; then
+        URL="$(git -C "${DIR}" remote get-url origin)"
+        echo -e "Saving DIR:\e[33m${DIR}\e[m, URL:\e[32m${URL}\e[m" >/dev/tty
+        echo -e "${DIR}\t --> \t${URL}"
+    fi
 done >"${BACKUP_FILE}"
