793e5e55556081961d90ec76f0eabe07b9e18514
Daniele Muffato <danixgithub1@gmail.com>
2024-07-22 15:36:16 +0200

    autosave also git branch for git repos tracked

--- bigfiles/repos/restore_git_repos.sh
+++ bigfiles/repos/restore_git_repos.sh
@@ -11,12 +11,13 @@ touch "${TMP_FILE}"
 while read -r line; do
     DIR="$(echo "${line}" | awk '{print $1}')"
     URL="$(echo "${line}" | awk '{print $3}')"
-    EMAIL="$(echo "${line}" | awk '{print $5}')"
+    BRANCH="$(echo "${line}" | awk '{print $5}')"
+    EMAIL="$(echo "${line}" | awk '{print $7}')"
     if ! [[ -d ${DIR} ]]; then
-        echo -en "Do you want to clone \e[33m$URL\e[m in \e[32m$DIR\e[m (email:\e[31m$EMAIL\e[m) [Y/n] ? "
+        echo -en "Do you want to clone \e[33m$URL\e[m in \e[32m$DIR\e[m (branch:\e[34m$BRANCH\e[m, email:\e[31m$EMAIL\e[m) [Y/n] ? "
         read -r answer </dev/tty
         if [[ ${answer,,} == 'y' ]]; then
-            echo "${DIR} ${URL} ${EMAIL}" >>"${TMP_FILE}"
+            echo "${DIR} ${URL} ${BRANCH} ${EMAIL}" >>"${TMP_FILE}"
         fi
     fi
 done <"$BACKUP_FILE"
@@ -25,10 +26,12 @@ done <"$BACKUP_FILE"
 while read -r line; do
     DIR="$(echo "${line}" | awk '{print $1}')"
     URL="$(echo "${line}" | awk '{print $2}')"
-    EMAIL="$(echo "${line}" | awk '{print $3}')"
-    echo -e "Cloning \e[33m$URL\e[m in \e[32m$DIR\e[m"
+    BRANCH="$(echo "${line}" | awk '{print $3}')"
+    EMAIL="$(echo "${line}" | awk '{print $4}')"
+    echo -e "Cloning \e[33m$URL\e[m in \e[32m$DIR\e[m (branch:\e[34m$BRANCH\e[m, email:\e[31m$EMAIL\e[m)"
     git clone "${URL}" "${DIR}"
     git -C "${DIR}" config user.email "${EMAIL}"
+    git -C "${DIR}" checkout "${BRANCH}"
 done <"$TMP_FILE"
 
 rm "${TMP_FILE}"
--- bigfiles/repos/save_git_repos.sh
+++ bigfiles/repos/save_git_repos.sh
@@ -7,9 +7,10 @@ BACKUP_FILE="${SCRIPT_DIR}/git_repos.txt"
 find /personal/repos/ -iname .git | while read -r dir; do
     DIR="$(dirname "${dir}")"
     EMAIL="$(git -C "${DIR}" config user.email)"
+    BRANCH="$(git -C "${DIR}" rev-parse --abbrev-ref HEAD)"
     if git -C "${DIR}" remote get-url origin &>/dev/null; then
         URL="$(git -C "${DIR}" remote get-url origin)"
-        echo -e "Saving DIR:\e[33m${DIR}\e[m, URL:\e[32m${URL}\e[m, EMAIL:\e[31m${EMAIL}\e[m" >/dev/tty
-        echo -e "${DIR}\t --> \t${URL}\t --> \t${EMAIL}"
+        echo -e "Saving DIR:\e[33m${DIR}\e[m, URL:\e[32m${URL}\e[m, BRANCH:\e[34m${BRANCH}\e[m, EMAIL:\e[31m${EMAIL}\e[m" >/dev/tty
+        echo -e "${DIR}\t --> \t${URL}\t --> \t${BRANCH} --> \t${EMAIL}"
     fi
 done >"${BACKUP_FILE}"
