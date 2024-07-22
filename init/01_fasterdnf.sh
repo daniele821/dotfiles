d0022a96d664fc53f19bd3166ee5647ed6ec94d2
Daniele Muffato <danixgithub1@gmail.com>
2024-07-22 11:47:46 +0200

    removed kde config script (buggy -> do manually!)

--- init/01_fasterdnf.sh
+++ init/01_fasterdnf.sh
@@ -1,15 +1,15 @@
 #!/bin/env bash
 
 function dnf_config() {
-	CONFIG="${1}"
-	PAIR="${1}${2}"
-	if [[ "$(grep -c "${CONFIG}" /etc/dnf/dnf.conf)" -gt 0 ]]; then
-		echo "dnf setting already exists: '$(grep "${CONFIG}" /etc/dnf/dnf.conf)'"
-	else
-		echo -en "Do you want to modify dnf configuration to set ${PAIR} [y/n]? "
-		read -r answer </dev/tty
-		[[ "${answer,,:0:1}" == "y" ]] && echo "${PAIR}" | sudo tee -a /etc/dnf/dnf.conf
-	fi
+    CONFIG="${1}"
+    PAIR="${1}${2}"
+    if [[ "$(grep -c "${CONFIG}" /etc/dnf/dnf.conf)" -gt 0 ]]; then
+        echo "dnf setting already exists: '$(grep "${CONFIG}" /etc/dnf/dnf.conf)'"
+    else
+        echo -en "Do you want to modify dnf configuration to set ${PAIR} [y/n]? "
+        read -r answer </dev/tty
+        [[ "${answer,,:0:1}" == "y" ]] && echo "${PAIR}" | sudo tee -a /etc/dnf/dnf.conf
+    fi
 }
 
 dnf_config "max_parallel_downloads=" "10"
--- init/03_localop.sh
+++ init/03_localop.sh
@@ -2,36 +2,41 @@
 
 # utility functions
 function ask_user() {
-	echo -n "${*} [Y/n] ? "
-	read -r answer </dev/tty
-	[[ "${answer,,}" == 'y' ]]
+    echo -n "${*} [Y/n] ? "
+    read -r answer </dev/tty
+    [[ "${answer,,}" == 'y' ]]
 }
 
 # mandatory init operations
 if ! [[ -d "${PERSONAL_DIR}" ]]; then
-	sudo mkdir -p /personal/{data,repos} || exit 1
-	sudo chown "${USER}":"${USER}" /personal/{data,repos}
-	echo "created personal directory in /personal"
+    sudo mkdir -p /personal/{data,repos} || exit 1
+    sudo chown "${USER}":"${USER}" /personal/{data,repos}
+    echo "created personal directory in /personal"
 fi
 
 # restore backup files
 if ask_user 'Do you want to restore all backup files'; then
-	SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
-	SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
-	"${SCRIPT_DIR}/../autosaver.sh" restore
+    SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
+    SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
+    "${SCRIPT_DIR}/../autosaver.sh" restore
 fi </dev/tty
 
 # create ssh keys for github
 if ask_user 'Do you really want to create new ssh keys'; then
-	USERS=(daniele821 danix1234)
-	for user in "${USERS[@]}"; do
-		ssh-keygen -t ed25519 -f ~/.ssh/id_"${user}"
-	done
+    USERS=(daniele821 danix1234)
+    for user in "${USERS[@]}"; do
+        ssh-keygen -t ed25519 -f ~/.ssh/id_"${user}"
+    done
 fi </dev/tty
 
 # enable wireshark for current user
 if ask_user 'Do you really want to enable wireshark'; then
-	sudo usermod -a -G wireshark "${USER}"
+    sudo usermod -a -G wireshark "${USER}"
 fi </dev/tty
+
+# disable login manager
+if ask_user 'Do you want to disable sddm'; then
+    sudo systemctl disable sddm.service
+fi
 
 exit 0
