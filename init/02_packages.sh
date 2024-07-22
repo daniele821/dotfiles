4a7f58846ad230915adbf05566acd973e5830296
daniele821 <137685223+daniele821@users.noreply.github.com>
2024-07-22 17:40:58 +0200

    update to autoremoved packages

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -8,7 +8,7 @@
     sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr starship htop libreoffice-langpack-it wireshark mpv libsixel-utils jq hyprland gammastep waybar hyprlock brightnessctl
 
     # uninstall bloat
-    sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool kwalletmanager krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel libreport kitty
+    sudo dnf --assumeyes remove plasma-discover plasma-welcome plasma-vault plasma-drkonqi im-chooser kaddressbook kontact kde-connect kgpg ktnef kmines kmahjongg kmail kmousetool krdc kmouth akregator gnome-abrt dragon elisa-player neochat krdc korganizer krfb khelpcenter skanpage kpat kfind mediawriter kamoso kcharselect kolourpaint spectacle nwg-panel libreport kitty
 
     # upgrade everything #
     sudo dnf --assumeyes upgrade
