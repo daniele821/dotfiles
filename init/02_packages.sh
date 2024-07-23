8aa59cbdadb0d000b81df2e53329d97c6fae55f5
Daniele Muffato <danixgithub1@gmail.com>
2024-07-23 21:51:17 +0200

    autoinstall ffmpeg (fixes most multimedia problems)

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -3,16 +3,17 @@
 {
     # add needed copr
     sudo dnf --assumeyes copr enable atim/starship
+    sudo dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    sudo dnf -y install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-    # install utilities (mpv or haruna for media playing)
+    # install utilities
+    sudo dnf --assumeyes install ffmpeg --allow-eraising
     sudo dnf --assumeyes install neovim gcc fd-find ripgrep zoxide bat lsd tldr starship htop libreoffice-langpack-it wireshark mpv libsixel-utils jq hyprland gammastep waybar hyprlock brightnessctl
 
     # upgrade everything #
     sudo dnf --assumeyes upgrade
 
     # safe uninstall bloat
-    sudo dnf --assumeyes remove kaddressbook kontact kde-connect kgpg kitty nwg-panel mediawriter kamoso kcharselect  spectacle kmines kmahjongg kmail akregator dragon elisa-player neochat im-chooser kfind khelpcenter kmousetool libreport skanpage gnome-abrt plasma-drkonqi korganizer kpat kolourpaint kmouth plasma-discover plasma-welcome krdc krfb plasma-vault
+    sudo dnf --assumeyes remove kaddressbook kontact kde-connect kgpg kitty nwg-panel mediawriter kamoso kcharselect spectacle kmines kmahjongg kmail akregator dragon elisa-player neochat im-chooser kfind khelpcenter kmousetool libreport skanpage gnome-abrt plasma-drkonqi korganizer kpat kolourpaint kmouth plasma-discover plasma-welcome krdc krfb plasma-vault
 
 } </dev/tty
-
-
