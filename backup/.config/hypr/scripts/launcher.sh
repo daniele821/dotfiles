64eb3ce60ebc1ad331cbf61e42b20c06faf2dde6
Daniele Muffato <danixgithub1@gmail.com>
2024-07-31 19:52:56 +0200

    added multiconfigs in hyprland

--- backup/.config/hypr/scripts/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# this script takes care of launching of scripts needed by hyprland
-# in hyprland config, just exec-once this script!
-
-SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
-SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
-
-"${SCRIPT_DIR}"/events.sh
