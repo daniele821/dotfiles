156d8bf9b3d774416a6532da1b57622e2a9074aa
Daniele Muffato <danixgithub1@gmail.com>
2024-08-03 18:51:33 +0200

    hyprland removed double conf

--- backup/.config/hypr/configs/lib/multi/scripts/swapfocus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# swap between tiled or floating windows
-
-if "$(hyprctl -j activewindow | jq .floating)"; then
-    hyprctl dispatch cyclenext tile
-else
-    hyprctl dispatch cyclenext float && hyprctl dispatch alterzorder top
-fi
