a1b45e625b82077e39a2b8dab476fe10145c9734
Daniele Muffato <danixgithub1@gmail.com>
2024-08-12 19:40:46 +0200

    implemented hacky way to detect if currently in special workspace in hyprland script

--- backup/.config/hypr/scripts/empty.sh
+++ backup/.config/hypr/scripts/empty.sh
@@ -8,17 +8,23 @@ EXTRA_WS="$(hyprctl workspaces -j | jq ".[] | select(.id >= $FIRST_EMPTY) | .id"
 PREV_WS="$(hyprctl workspaces -j | jq ".[] | select(.id >= $FIRST_EMPTY) | select(.id < $CURRENT_ID) | .id" | sort -n | tail -1)"
 NEXT_WS="$(hyprctl workspaces -j | jq ".[] | select(.id >= $FIRST_EMPTY) | select(.id > $CURRENT_ID) | .id" | sort -n | head -1)"
 EMPTY="${FIRST_EMPTY}"
+SPECIAL_WS="$(hyprctl clients -j | jq '.[] | select(.focusHistoryID == 0) | .workspace.id < 0')"
 for n in ${EXTRA_WS}; do [[ $n -eq $EMPTY ]] && ((EMPTY = EMPTY + 1)); done
 
 case "$1" in
 "goto")
-    [[ "$CURRENT_ID" -ge "$FIRST_EMPTY" && "$CURRENT_WINDOWS" -eq 0 ]] || hyprctl dispatch workspace "${EMPTY}"
+    [[ "$CURRENT_ID" -lt "$EMPTY" && "$CURRENT_ID" -ge "$FIRST_EMPTY" && "$CURRENT_WINDOWS" -eq 0 ]] && EMPTY="$CURRENT_ID"
+    hyprctl dispatch workspace "${EMPTY}"
     ;;
 "moveto")
-    [[ "$CURRENT_ID" -ge "$FIRST_EMPTY" && "$CURRENT_WINDOWS" -le 1 ]] || hyprctl dispatch movetoworkspace "${EMPTY}"
+    eval "${SPECIAL_WS}" && [[ "$CURRENT_ID" -lt "$EMPTY" && "$CURRENT_ID" -ge "$FIRST_EMPTY" && "$CURRENT_WINDOWS" -eq 0 ]] && EMPTY="$CURRENT_ID"
+    ! eval "${SPECIAL_WS}" && [[ "$CURRENT_ID" -lt "$EMPTY" && "$CURRENT_ID" -ge "$FIRST_EMPTY" && "$CURRENT_WINDOWS" -le 1 ]] && EMPTY="$CURRENT_ID"
+    hyprctl dispatch movetoworkspace "${EMPTY}"
     ;;
 "sendto")
-    [[ "$CURRENT_ID" -ge "$FIRST_EMPTY" && "$CURRENT_WINDOWS" -le 1 ]] || hyprctl dispatch movetoworkspacesilent "${EMPTY}"
+    eval "${SPECIAL_WS}" && [[ "$CURRENT_ID" -lt "$EMPTY" && "$CURRENT_ID" -ge "$FIRST_EMPTY" && "$CURRENT_WINDOWS" -eq 0 ]] && EMPTY="$CURRENT_ID"
+    ! eval "${SPECIAL_WS}" && [[ "$CURRENT_ID" -lt "$EMPTY" && "$CURRENT_ID" -ge "$FIRST_EMPTY" && "$CURRENT_WINDOWS" -le 1 ]] && EMPTY="$CURRENT_ID"
+    hyprctl dispatch movetoworkspacesilent "${EMPTY}"
     ;;
 "left")
     [[ -z "$EXTRA_WS" ]] && hyprctl dispatch workspace "${EMPTY}" && exit 0
