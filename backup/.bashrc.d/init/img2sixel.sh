4e21c858b3aa6d6e3644205ba5e7c0e249c16a0d
Daniele Muffato <danixgithub1@gmail.com>
2024-08-12 23:22:02 +0200

    kitty fixes

--- backup/.bashrc.d/init/img2sixel.sh
+++ backup/.bashrc.d/init/img2sixel.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-function preview-all() {
-    [[ "$#" -gt 25 ]] && echo 'too many images at once' && return 1
-    COLS="$(tput cols)"
-    ((COLS = COLS * 5))
-    for elem in "${@}"; do
-        echo "${elem}"
-        if [[ "$TERM" =~ kitty ]]; then
-            kitten icat "${elem}"
-        else
-            img2sixel -w $COLS "${elem}"
-        fi
-    done | less -r
-}
-
 function preview() {
-    [[ "$#" -gt 25 ]] && echo 'too many images at once' && return 1
-    for file in "${@}"; do
-        preview-all "${file}"
-    done
+    if [[ "$TERM" =~ kitty ]]; then
+        kitten icat "${1}"
+    else
+        COLS="$(tput cols)"
+        ((COLS = COLS * 8))
+        img2sixel -w "$COLS" "${1}"
+    fi | less -r
 }
