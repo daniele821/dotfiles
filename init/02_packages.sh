9222a9e32bb5ea563df3bdbc858276da95df7978
Daniele Muffato <danixgithub1@gmail.com>
2024-08-19 16:55:56 +0200

    removed sixel package

--- init/02_packages.sh
+++ init/02_packages.sh
@@ -13,7 +13,7 @@ powerprofilesctl set performance
     sudo dnf --assumeyes install --allowerasing ffmpeg
     sudo dnf --assumeyes install neovim gcc fd-find ripgrep
     sudo dnf --assumeyes install zoxide bat lsd tldr starship htop direnv fastfetch
-    sudo dnf --assumeyes install libreoffice-langpack-it wireshark mpv libsixel-utils
+    sudo dnf --assumeyes install libreoffice-langpack-it wireshark mpv
     sudo dnf --assumeyes install jq hyprland gammastep waybar hyprlock brightnessctl kitty
 
     # upgrade everything
