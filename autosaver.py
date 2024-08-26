464efff30bd8092c6913e77d9567adc8536077db
Daniele Muffato <danixgithub1@gmail.com>
2024-08-26 20:44:25 +0200

    small fixes

--- autosaver.py
+++ autosaver.py
@@ -13,14 +13,17 @@ DIRS = {"backup": SCRIPT_DIR + "/backup",
 FILES = {"track": DIRS["config"] + "/files_to_track.txt"}
 
 
-def color(clr, str, print=True):
+def color(clr, str, output=True):
     clr_str = "\033["
     match clr:
-        case "err" | "error": clr_str += "1;31m"
+        case "err": clr_str += "1;31m"
         case "file": clr_str += "1;34m"
         case "msg": clr_str += "1;33m"
     res = clr_str + str + "\033[m"
-    return print(res) if print else res
+    if output:
+        print(res, end="")
+    else:
+        return res
 
 
 def parse_options():
