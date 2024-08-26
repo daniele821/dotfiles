2783a0fd2980dd4f79e20eb2baec97609f9dcd33
Daniele Muffato <danixgithub1@gmail.com>
2024-08-26 19:17:25 +0200

    wip

--- autosaver.py
+++ autosaver.py
@@ -4,3 +4,8 @@ import os
 
 SCRIPT_PATH = os.path.realpath(__file__)
 SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
+DIRS = {"backup": SCRIPT_DIR + "/backup",
+        "config": SCRIPT_DIR + "/config",
+        "init": SCRIPT_DIR + "/init",
+        "bigfiles": SCRIPT_DIR + "bigfiles"}
+FILES = {"track": DIRS["config"] + "/files_to_track.txt"}
