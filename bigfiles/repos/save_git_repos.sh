b9566603e52d173e2180ac373d67809b33ab6d8e
Daniele Muffato <danixgithub1@gmail.com>
2024-08-27 19:23:32 +0200

    add check in git repo save script to exclude worktree repos from being saved to file

--- bigfiles/repos/save_git_repos.sh
+++ bigfiles/repos/save_git_repos.sh
@@ -5,13 +5,15 @@ SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 BACKUP_FILE="${SCRIPT_DIR}/git_repos.txt"
 
 find /personal/repos/ -iname .git | while read -r dir; do
-    DIR="$(dirname "${dir}")"
-    EMAIL="$(git -C "${DIR}" config user.email)"
-    BRANCH="$(git -C "${DIR}" rev-parse --abbrev-ref HEAD)"
-    if git -C "${DIR}" remote get-url origin &>/dev/null; then
-        URL="$(git -C "${DIR}" remote get-url origin)"
-        echo -e "Saving DIR:\e[33m${DIR}\e[m, URL:\e[32m${URL}\e[m, BRANCH:\e[34m${BRANCH}\e[m, EMAIL:\e[31m${EMAIL}\e[m" >/dev/tty
-        echo -e "${DIR}\n${URL}\n${BRANCH}\n${EMAIL}"
-        echo "---------------------------------------------------------------------"
+    if [[ -d "${dir}" ]]; then # if .git is a file, then the directory is a worktree dir
+        DIR="$(dirname "${dir}")"
+        EMAIL="$(git -C "${DIR}" config user.email)"
+        BRANCH="$(git -C "${DIR}" rev-parse --abbrev-ref HEAD)"
+        if git -C "${DIR}" remote get-url origin &>/dev/null; then
+            URL="$(git -C "${DIR}" remote get-url origin)"
+            echo -e "Saving DIR:\e[33m${DIR}\e[m, URL:\e[32m${URL}\e[m, BRANCH:\e[34m${BRANCH}\e[m, EMAIL:\e[31m${EMAIL}\e[m" >/dev/tty
+            echo -e "${DIR}\n${URL}\n${BRANCH}\n${EMAIL}"
+            echo "---------------------------------------------------------------------"
+        fi
     fi
 done >"${BACKUP_FILE}"
