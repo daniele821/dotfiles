0f6ccfd1a365c711c819e278d6815bb96d1d1556
Daniele Muffato <danixgithub1@gmail.com>
2024-09-02 16:58:50 +0200

    small fix in git repo saver script

--- bigfiles/repos/save_git_repos.sh
+++ bigfiles/repos/save_git_repos.sh
@@ -7,11 +7,11 @@ BACKUP_FILE="${SCRIPT_DIR}/git_repos.txt"
 TMP_FILE="$(mktemp /tmp/gitreposXXXXXXXXXXXXXXXXXXXXX.tmp)"
 
 find /personal/repos/ -iname .git | while read -r dir; do
+    DIR="$(dirname "${dir}")"
+    EMAIL="$(git -C "${DIR}" config user.email)"
+    BRANCH="$(git -C "${DIR}" rev-parse --abbrev-ref HEAD)"
+    URL="$(git -C "${DIR}" remote get-url "$(git -C "${DIR}" remote)" 2>/dev/null)"
     if [[ -d "${dir}" ]]; then # if .git is a file, then the directory is a worktree dir
-        DIR="$(dirname "${dir}")"
-        EMAIL="$(git -C "${DIR}" config user.email)"
-        BRANCH="$(git -C "${DIR}" rev-parse --abbrev-ref HEAD)"
-        URL="$(git -C "${DIR}" remote get-url "$(git -C "${DIR}" remote)" 2>/dev/null)"
         if [[ -n "${URL}" && "${BRANCH}" != "HEAD" ]]; then
             echo -e "Saving DIR:\e[33m${DIR}\e[m, URL:\e[32m${URL}\e[m, BRANCH:\e[34m${BRANCH}\e[m, EMAIL:\e[31m${EMAIL}\e[m" >/dev/tty
             echo -e "${DIR}\n${URL}\n${BRANCH}\n${EMAIL}"
