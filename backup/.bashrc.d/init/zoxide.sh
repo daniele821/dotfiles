b7b716f07e6a8cc594b5575ffcffdb4f83719295
Daniele Muffato <danixgithub1@gmail.com>
2024-09-05 23:00:56 +0200

    zoxide: undone manual adding dirs

--- backup/.bashrc.d/init/zoxide.sh
+++ backup/.bashrc.d/init/zoxide.sh
@@ -1,33 +1,13 @@
 #!/bin/env bash
 
 ### startup program ###
-eval "$(zoxide init --hook none --no-cmd bash)"
+eval "$(zoxide init --no-cmd bash)"
 
 ## startup programs configs ##
 function __zoxide_euristically__() {
-    local -r oldpwd="${PWD}"
-    if ! cd "$@" &>/dev/null; then
-        if (__zoxide_z "$@" &>/dev/null && zoxide add "${PWD}"); then
-            __zoxide_z "$@" &>/dev/null
-        fi
-        [[ "${oldpwd}" != "${PWD}" ]]
-    fi
-}
-function __zoxide_add_paths__() {
-    if [[ "$#" -eq 0 ]]; then
-        zoxide add "${PWD}"
-        return 0
-    fi
-    for path in "${@}"; do
-        path="$(realpath "${path}")"
-        if [[ -d "${path}" ]]; then
-            zoxide add "${path}"
-        fi
-    done
-    return 0
+    cd "$@" &>/dev/null || __zoxide_z "$@" &>/dev/null
 }
 alias cd='__zoxide_euristically__'
-alias ca='__zoxide_add_paths__'
 alias ce='zoxide edit'
 alias ci='__zoxide_zi'
 alias cl='zoxide query -ls'
