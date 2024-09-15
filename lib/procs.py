504efc8fe750639e3fe7579898cd1e7ae4bad21f
Daniele Muffato <danixgithub1@gmail.com>
2024-09-15 06:46:31 +0200

    WIP: adding more to procs module

--- lib/procs.py
+++ lib/procs.py
@@ -4,15 +4,15 @@ from subprocess import run
 
 
 def diff(old, new):
-    return run(["diff", "--color", "-u", old, new])
+    run(["diff", "--color", "-u", old, new])
 
 
 def edit(file):
-    return run(["nvim", file])
+    run(["nvim", file])
 
 
-def execute(file):
-    return run(["nvim", file])
+def run_and_get_status(file):
+    return run(["nvim", file]).returncode != 0
 
 
 def git_pull(gitdir):
@@ -23,12 +23,27 @@ def git_push(gitdir):
     run(["git", "-C", gitdir, "push"])
 
 
-def git_restore(gitdir):
+def git_status(gitdir):
+    run(["git", "-C", dir, "status", "-su"])
+
+
+def git_diff(gitdir, reverse=False):
+    cmd = ["git", "-C", gitdir, "diff", "HEAD", "--diff-filter=adcr"]
+    run(cmd + ["-R"] if reverse else cmd)
+
+
+def git_restore_all(gitdir):
     run(["git", "-C", gitdir, "reset", "HEAD"])
     run(["git", "-C", gitdir, "restore", "--staged", gitdir])
     run(["git", "-C", gitdir, "restore", gitdir])
     run(["git", "-C", gitdir, "clean", "-fdq"])
 
 
-def git_commit(gitdir):
-    pass
+def git_commit_all(gitdir, commit_msg):
+    run(["git", "-C", gitdir, "add", gitdir])
+    run(["git", "-C", gitdir, "commit", "-m", commit_msg])
+
+
+def has_git_changed(gitidir):
+    cmd = ['git', '-C', dir, 'status', '-s']
+    return run(cmd, capture_output=True, text=True).stdout.strip()
