1628d945b6087fc6690fd22976de18051cc268af
Daniele Muffato <danixgithub1@gmail.com>
2024-09-15 05:01:18 +0200

    WIP: adding more to procs module

--- lib/procs.py
+++ lib/procs.py
@@ -13,3 +13,22 @@ def edit(file):
 
 def execute(file):
     return run(["nvim", file])
+
+
+def git_pull(gitdir):
+    run(["git", "-C", gitdir, "pull"])
+
+
+def git_push(gitdir):
+    run(["git", "-C", gitdir, "push"])
+
+
+def git_restore(gitdir):
+    run(["git", "-C", gitdir, "reset", "HEAD"])
+    run(["git", "-C", gitdir, "restore", "--staged", gitdir])
+    run(["git", "-C", gitdir, "restore", gitdir])
+    run(["git", "-C", gitdir, "clean", "-fdq"])
+
+
+def git_commit(gitdir):
+    pass
