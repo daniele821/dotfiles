1066a2c989d05172ce97fa2580bf99ce9640412e
Daniele Muffato <danixgithub1@gmail.com>
2024-10-20 17:20:46 +0200

    removed script to reboot automatically

--- run/04_reboot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-# apply all changes
-echo "Warning: to apply changes, reboot is required"
-echo -n "Do you still wish to proceed [y/n]? "
-read -r answer </dev/tty
-[[ "${answer,,:0:1}" == "y" ]] || exit 0
-reboot
