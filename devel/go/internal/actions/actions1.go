d4afb69ef77c2c7756b8527d165f6c2bd38cc43c
Daniele Muffato <danixgithub1@gmail.com>
2024-10-26 09:27:58 +0200

    wip backup action

--- devel/go/internal/actions/actions1.go
+++ devel/go/internal/actions/actions1.go
@@ -5,9 +5,86 @@ import (
 	"autosaver/internal/utils"
 	"fmt"
 	"path/filepath"
+	"slices"
 )
 
-func backupAction(flag *configs.Flag) {}
+func backupAction(flag *configs.Flag) {
+	act := flag.GetActionFlag()
+	actSave := act == configs.ActSave
+	actBackup := act == configs.ActBackup
+	optToggle := flag.HasOptionFlag(configs.OptToggle)
+	optDiff := flag.HasOptionFlag(configs.OptDiff)
+	optForce := flag.HasOptionFlag(configs.OptForce)
+	optVerbose := flag.HasOptionFlag(configs.OptVerbose)
+	trackedFiles := loadConf(fileTrack)
+	notdiffFiles := loadConf(fileNotdiff)
+	allFiles := append(trackedFiles, notdiffFiles...)
+	slices.Sort(allFiles)
+	allFiles = slices.Compact(allFiles)
+
+	println(actSave, actBackup, optToggle, optDiff, optVerbose, optForce)
+
+	for _, file := range allFiles {
+		homeFile := filepath.Join(configs.Home, file)
+		backupFile := filepath.Join(dirBackup, file)
+		isHomeFile := utils.IsRegularFile(homeFile)
+		isBackupFile := utils.IsRegularFile(backupFile)
+		switch {
+		case !isHomeFile && isBackupFile:
+			// todo
+		case isHomeFile && !isBackupFile:
+			// todo
+		case isHomeFile && isBackupFile && (optToggle || !slices.Contains(notdiffFiles, file)):
+			if utils.ProcessFilesDiffer(homeFile, backupFile) {
+				// todo
+			}
+		}
+	}
+
+	//	    match os.path.isfile(ofile), os.path.isfile(bfile):
+	//	        case True, False:
+	//	            fout(ofile, "backup file", "is missing")
+	//	            if act_save:
+	//	                if ask_user(qmsg("create", "backup"), opts):
+	//	                    copy_file(ofile, bfile)
+	//	            if act_backup and opt_force:
+	//	                if ask_user(qmsg("delete", "original", "[DANGER] "), opts):
+	//	                    delete_file(ofile)
+	//	        case False, True:
+	//	            fout(ofile, "original file", "is missing")
+	//	            if act_save:
+	//	                if ask_user(qmsg("delete", "backup"), opts):
+	//	                    delete_file(bfile, True)
+	//	            if act_backup:
+	//	                if ask_user(qmsg("create", "original"), opts):
+	//	                    copy_file(bfile, ofile)
+	//	        case True, True:
+	//	            if opt_toggle or file not in notdiff:
+	//	                if are_files_different(ofile, bfile):
+	//	                    fout(ofile, "original and backup files", "differ")
+	//	                    if opt_diff:
+	//	                        old = ofile if act_backup else bfile
+	//	                        new = bfile if act_backup else ofile
+	//	                        diff(old, new)
+	//	                    if act_save:
+	//	                        if ask_user(qmsg("update", "backup"), opts):
+	//	                            copy_file(ofile, bfile)
+	//	                    if act_backup:
+	//	                        if ask_user(qmsg("update", "original"), opts):
+	//	                            copy_file(bfile, ofile)
+
+	//	def qmsg(act, on,  prefix=""):
+	//	    res = prefix + "Do you really want to " + act + " " + on + " file? "
+	//	    return color("msg", res)
+	//
+	//	def fout(file, on=None, stat=None):
+	//	    msg_file = color("file", file)
+	//	    if opt_verbose and on and stat:
+	//	        print(msg_file + " : " + on + " " + stat)
+	//	    else:
+	//	        print(msg_file)
+
+}
 
 func untrackedAction(flag *configs.Flag) {
 	autoAnswer := autoAnswer(flag)
@@ -68,63 +145,3 @@ func commitAction(flag *configs.Flag) {
 		utils.ProcessGitPush(dirScript)
 	}
 }
-
-// def backup_files(act, opts):
-//     odir = HOME
-//     bdir = DIRS["backup"]
-//     act_save = act == ACTIONS.SAVE
-//     act_backup = act == ACTIONS.BACKUP
-//     opt_toggle = FLAGS.TOGGLE in opts
-//     opt_diff = FLAGS.DIFF in opts
-//     opt_force = FLAGS.FORCE in opts
-//     opt_verbose = FLAGS.VERBOSE in opts
-//     tracked = load_config(FILES["track"])
-//     notdiff = load_config(FILES["notdiff"])
-//     all = sorted(tracked | notdiff)
-//
-//     def qmsg(act, on,  prefix=""):
-//         res = prefix + "Do you really want to " + act + " " + on + " file? "
-//         return color("msg", res)
-//
-//     def fout(file, on=None, stat=None):
-//         msg_file = color("file", file)
-//         if opt_verbose and on and stat:
-//             print(msg_file + " : " + on + " " + stat)
-//         else:
-//             print(msg_file)
-//
-//     for file in all:
-//         ofile = os.path.join(odir, file)
-//         bfile = os.path.join(bdir, file)
-//         match os.path.isfile(ofile), os.path.isfile(bfile):
-//             case True, False:
-//                 fout(ofile, "backup file", "is missing")
-//                 if act_save:
-//                     if ask_user(qmsg("create", "backup"), opts):
-//                         copy_file(ofile, bfile)
-//                 if act_backup and opt_force:
-//                     if ask_user(qmsg("delete", "original", "[DANGER] "), opts):
-//                         delete_file(ofile)
-//             case False, True:
-//                 fout(ofile, "original file", "is missing")
-//                 if act_save:
-//                     if ask_user(qmsg("delete", "backup"), opts):
-//                         delete_file(bfile, True)
-//                 if act_backup:
-//                     if ask_user(qmsg("create", "original"), opts):
-//                         copy_file(bfile, ofile)
-//             case True, True:
-//                 if opt_toggle or file not in notdiff:
-//                     if are_files_different(ofile, bfile):
-//                         fout(ofile, "original and backup files", "differ")
-//                         if opt_diff:
-//                             old = ofile if act_backup else bfile
-//                             new = bfile if act_backup else ofile
-//                             diff(old, new)
-//                         if act_save:
-//                             if ask_user(qmsg("update", "backup"), opts):
-//                                 copy_file(ofile, bfile)
-//                         if act_backup:
-//                             if ask_user(qmsg("update", "original"), opts):
-//                                 copy_file(bfile, ofile)
-//
