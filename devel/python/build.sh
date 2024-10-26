e74628d5a2f9ef0db5852e1159f4607dbddd2772
Daniele Muffato <danixgithub1@gmail.com>
2024-10-27 01:51:15 +0200

    updated build script

--- devel/python/build.sh
+++ devel/python/build.sh
@@ -4,6 +4,13 @@
 SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 
+changes="$(git -C "${SCRIPT_DIR}/../.." status -su | wc -l)"
+if [[ "${changes}" == "0" ]]; then
+    echo -n 'no changes were made, Do you really want to continue anyway? '
+    read -r answer
+    [[ "${answer,,}" != "y" ]] && exit 0
+fi
+
 cp "${SCRIPT_DIR}/autosaver" "${SCRIPT_DIR}/../../autosaver"
 
 if [[ "$1" == "-y" ]]; then
