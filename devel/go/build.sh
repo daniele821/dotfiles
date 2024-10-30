159bcb118e31c2be7a3420870d9b09213dc85675
Daniele Muffato <danixgithub1@gmail.com>
2024-10-30 12:12:52 +0100

    go build script now doesn't create a tmpdir

--- devel/go/build.sh
+++ devel/go/build.sh
@@ -4,12 +4,7 @@
 SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 
-TMPDIR="$(mktemp -d)"
-
 cd "${SCRIPT_DIR}" || exit 1
 
-go build -o "${TMPDIR}/" "${SCRIPT_DIR}/cmd/autosaver/main.go"
-mv "${TMPDIR}/"* "${SCRIPT_DIR}/autosaver"
+go build -o "${SCRIPT_DIR}/autosaver" "${SCRIPT_DIR}/cmd/autosaver/main.go"
 cp "${SCRIPT_DIR}/autosaver" "${SCRIPT_DIR}/../../autosaver"
-
-rmdir "${TMPDIR}"
