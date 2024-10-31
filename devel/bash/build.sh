6264ff02230d59eb86e80bff11d5a78954aace8b
Daniele Muffato <danixgithub1@gmail.com>
2024-10-31 13:46:57 +0100

    moved bash script from separate branch, to here

---
+++ devel/bash/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# build autosaver binary file
+SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
+
+cp "${SCRIPT_DIR}/autosaver" "${SCRIPT_DIR}/../../autosaver"
