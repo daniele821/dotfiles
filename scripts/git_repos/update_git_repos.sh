ed441a8b2eba302e7fc551686e16f5cd5df8eb1f
Daniele Muffato <danixgithub1@gmail.com>
2024-11-10 03:03:55 +0100

    added script to update all git repos

---
+++ scripts/git_repos/update_git_repos.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
+BACKUP_FILE="${SCRIPT_DIR}/git_repos.txt"
+
+# early exit if no backup file is present
+[[ -f "${BACKUP_FILE}" ]] || exit 0
+
+# clone missing directories
+COUNTER=0
+while read -r line; do
+    ((COUNTER += 1))
+    case "$COUNTER" in
+    1) DIR="${line}" ;;
+    2) URL="${line}" ;;
+    3) ;;
+    4) ;;
+    5)
+        if [[ -d ${DIR} ]]; then
+            echo -e "Updating \e[33m$URL\e[m in \e[32m$DIR\e[m"
+            git -C "${DIR}" pull --ff-only
+        fi
+        COUNTER=0
+        ;;
+    *)
+        echo -e "\e[31merror: INVALID COUNTER VALUE\e[m"
+        exit 1
+        ;;
+    esac
+done <"$BACKUP_FILE"
+
+exit 0
