0fe3f79ccee0697ab9019c81a2ba49f34ba31102
Daniele Muffato <danixgithub1@gmail.com>
2024-11-17 16:14:31 +0100

    reinit

---
+++ backup/.bashrc.d/various.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export EDITOR="nvim"
+export PYTHONDONTWRITEBYTECODE="true"
+export GOPATH="$HOME/.local/share/go"
+
+eval "$(direnv hook bash)"
+
+function __exec_nohupped__() {
+    (: && nohup "$@" &>/dev/null &)
+}
+function run() {
+    __exec_nohupped__ "$@"
+}
+function open() {
+    for file in "${@}"; do __exec_nohupped__ xdg-open "$file"; done
+}
+
+complete -f preview
+complete -f open
+complete -c run
+
+alias ls='lsd --group-dirs first'
+alias la='ls -A'
+alias ll='ls -l'
+alias lla='ls -lA'
+alias tree='lsd --group-dirs first --tree'
+alias cat='bat'
+alias clear='printf "\033[2J\033[3J\033[1;1H"'
+
+unset command_not_found_handle
+for i in - {0..9}; do bind -r "\e$i"; done
+bind -x '"\C-l": clear'
---
+++ run/01_packages.sh
@@ -0,0 +1,33 @@
+#!/bin/env bash
+
+{
+    ### upgrade everything ###
+    sudo dnf --assumeyes upgrade
+
+    # basic stuff
+    sudo dnf --assumeyes remove kpat kmines kmahjongg
+    sudo dnf --assumeyes remove dragon elisa-player neochat
+    sudo dnf --assumeyes remove mediawriter akregator khelpcenter
+    sudo dnf --assumeyes remove kdebugsettings plasma-welcome kde-connect
+    sudo dnf --assumeyes remove kaddressbook kmail kfind
+    sudo dnf --assumeyes remove konsolea kjournald skanpage
+    sudo dnf --assumeyes remove abrt akonadi-server kmouth
+    sudo dnf --assumeyes remove krdc krfb krdp
+    sudo dnf --assumeyes install libreoffice-langpack-it mpv kitty
+    sudo dnf --assumeyes install zoxide bat ripgrep lsd neovim gcc tldr golang direnv htop
+
+    # starship copr
+    sudo dnf --assumeyes copr enable atim/starship
+    sudo dnf --assumeyes install starship
+
+    # enable rpm-fusion and install multimedia codecs
+    sudo dnf --assumeyes install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    sudo dnf --assumeyes install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+    sudo dnf --assumeyes install ffmpeg --allowerasing
+
+    ### remove unnecessary packages ###
+    sudo dnf --assumeyes autoremove
+
+} </dev/tty
+
+exit 0
