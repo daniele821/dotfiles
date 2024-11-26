b436e6222bd72f1d28b7783f767bb1903fc5eb68
Daniele Muffato <danixgithub1@gmail.com>
2024-11-26 13:41:18 +0100

    made checkhealth for bash and global variable which it uses read only

--- backup/.bashrc.d/01_init.sh
+++ backup/.bashrc.d/01_init.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-for init in "$HOME/.bashrc.d/init/"*; do
+export MISSING_PROGRAMS=()
+for init in ~/.bashrc.d/init/*; do
     bname="$(basename "$init")"
     name="${bname%%.*}"
     if command -v "$name" &>/dev/null; then
         # shellcheck disable=SC1090
         . "${init}"
     else
-        echo "$name is not installed"
+        export MISSING_PROGRAMS+=("${name}")
     fi
 done
+declare -r MISSING_PROGRAMS
+
+function checkhealth() {
+    [[ "${#MISSING_PROGRAMS[@]}" -eq 0 ]] && return 0
+    echo "The following programs are required for a full bash experience:"
+    for i in "${MISSING_PROGRAMS[@]}"; do
+        echo "- $i"
+    done
+}
