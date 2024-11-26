b8fe10a689df52ee2594adf8e44b0f74cd39abc1
Daniele Muffato <danixgithub1@gmail.com>
2024-11-26 12:40:53 +0100

    added function which does cleanups before prompts is drawn

--- backup/.bashrc.d/03_various.sh
+++ backup/.bashrc.d/03_various.sh
@@ -84,3 +84,15 @@ alias clear='printf "\033[2J\033[3J\033[1;1H"'
 unset command_not_found_handle
 for i in - {0..9}; do bind -r "\e$i"; done
 bind -x '"\C-l": clear'
+
+function __cleanup_prompt__() {
+    \builtin local -r retval="$?"
+    # set DISABLE_CLEANUP env var to disable this entire function
+    [[ -v DISABLE_CLEANUP ]] && return "$retval"
+    # force new line in terminal if previous output doesn't end with a newline
+    IFS='[;' read -p $'\e[6n' -d R -rs _ COLUMN LINE _
+    [[ "$LINE" -ne "1" ]] && echo
+    return "${retval}"
+}
+[[ -n "${PROMPT_COMMAND}" ]] && export PROMPT_COMMAND+=";"
+export PROMPT_COMMAND+="__cleanup_prompt__"
