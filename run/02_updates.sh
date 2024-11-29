c3c05ffceaeb6582de9e25ad11e29abe355d90bc
Daniele Muffato <danixgithub1@gmail.com>
2024-11-29 05:40:19 +0100

    improved run script

--- run/02_updates.sh
+++ run/02_updates.sh
@@ -13,15 +13,16 @@ function ask_user() {
         curl -sS https://starship.rs/install.sh | sh
     fi
 
-    # assure platform is x86, since binaries downloaded are specific for that platform
-    [[ "$(uname -m)" != "x86_64" ]] && echo "platform '$(uname -m)' not supported!" && exit 0
+    # downloads for only x86 platform
+    if [[ "$(uname -m)" == "x86_64" ]]; then
 
-    # download kitten for terminal image drawing protocol
-    if ask_user 'Do you really want to download kitten locally'; then
-        TMP_FILE="$(mktemp)"
-        wget https://github.com/kovidgoyal/kitty/releases/download/v0.37.0/kitten-linux-386 -O "${TMP_FILE}"
-        chmod +x "${TMP_FILE}"
-        sudo mv "${TMP_FILE}" /usr/local/bin/kitten
+        # download kitten for terminal image drawing protocol
+        if ask_user 'Do you really want to download kitten locally'; then
+            TMP_FILE="$(mktemp)"
+            wget https://github.com/kovidgoyal/kitty/releases/download/v0.37.0/kitten-linux-386 -O "${TMP_FILE}"
+            chmod +x "${TMP_FILE}"
+            sudo mv "${TMP_FILE}" /usr/local/bin/kitten
+        fi
     fi
 
 } </dev/tty
