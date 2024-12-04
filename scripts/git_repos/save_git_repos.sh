eede1e9523ad03c82f1d7b3c14de353c5092a305
Daniele Muffato <danixgithub1@gmail.com>
2024-12-04 18:28:46 +0100

    strongly simplified save script for git repos

--- scripts/git_repos/save_git_repos.sh
+++ scripts/git_repos/save_git_repos.sh
@@ -54,22 +54,14 @@ fi
         EMAIL="$(git -C "${DIR}" config user.email 2>/dev/null)"
         BRANCH="$(git -C "${DIR}" rev-parse --abbrev-ref HEAD 2>/dev/null)"
         URL="$(git -C "${DIR}" remote get-url "$(git -C "${DIR}" remote 2>/dev/null)" 2>/dev/null)"
-        if [[ -d "${dir}" ]]; then # if .git is a file, then the directory is a worktree dir
-            if [[ -n "${URL}" && "${BRANCH}" != "HEAD" ]]; then
-                echo -e "Saving DIR:\e[33m${DIR}\e[m, URL:\e[32m${URL}\e[m, BRANCH:\e[34m${BRANCH}\e[m, EMAIL:\e[31m${EMAIL}\e[m" >/dev/tty
-                echo -e "${DIR}\n${URL}\n${BRANCH}\n${EMAIL}"
-                echo "---------------------------------------------------------------------"
-            else
-                if [[ "${BRANCH}" == "HEAD" ]]; then
-                    echo -e "\e[33;1mWARNING: ${DIR} couldn't be parsed: head is detacthed!\e[m"
-                elif [[ -z "${URL}" ]]; then
-                    echo -e "\e[33;1mWARNING: ${DIR} couldn't be parsed: couldn't parse remote url\e[m"
-                else
-                    echo -e "\e[33;1mWARNING: ${DIR} couldn't be parsed!\e[m"
-                fi >/dev/tty
-            fi
-        else
+        if [[ ! -d "${dir}" ]]; then
             echo -e "\e[33;1mWARNING: ${DIR} is a worktree!\e[m" >/dev/tty
+        elif [[ -z "${URL}" ]]; then
+            echo -e "\e[33;1mWARNING: ${DIR} couldn't be parsed: couldn't parse remote url\e[m" >/dev/tty
+        else
+            echo -e "Saving DIR:\e[33m${DIR}\e[m, URL:\e[32m${URL}\e[m, BRANCH:\e[34m${BRANCH}\e[m, EMAIL:\e[31m${EMAIL}\e[m" >/dev/tty
+            echo -e "${DIR}\n${URL}\n${BRANCH}\n${EMAIL}"
+            echo "---------------------------------------------------------------------"
         fi
     done >>"${BACKUP_FILE}"
 done <"${CONFIG_FILE}"
