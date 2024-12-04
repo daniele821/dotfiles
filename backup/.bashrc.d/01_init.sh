475dc718b549414e31afbe78ab093a77dba5fd32
Daniele Muffato <danixgithub1@gmail.com>
2024-12-04 04:25:18 +0100

    removed checkhealth bash

--- backup/.bashrc.d/01_init.sh
+++ backup/.bashrc.d/01_init.sh
@@ -8,20 +8,3 @@ for init in ~/.bashrc.d/init/*; do
         . "${init}"
     fi
 done
-
-function checkhealth() {
-    declare MISSING=()
-    for init in ~/.bashrc.d/init/*; do
-        bname="$(basename "$init")"
-        name="${bname%%.*}"
-        if ! command -v "$name" &>/dev/null; then
-            MISSING+=("$name")
-        fi
-    done
-    [[ "${#MISSING[@]}" -eq "0" ]] && echo "All the needed programs are installed!" && return 0
-    echo "The following programs are required for a full bash experience:"
-    for prog in "${MISSING[@]}"; do
-        echo "- $prog"
-    done
-    return 1
-}
--- backup/.bashrc.d/init/zoxide.sh
+++ backup/.bashrc.d/init/zoxide.sh
@@ -7,3 +7,5 @@ alias cd='__zoxide_euristically__'
 function __zoxide_euristically__() {
     \cd "$@" &>/dev/null || __zoxide_z "$@" &>/dev/null
 }
+alias ci='__zoxide_zi'
+alias ce='zoxide edit'
