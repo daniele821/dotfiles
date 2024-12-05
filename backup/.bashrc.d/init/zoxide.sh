7e370a602f6ee15712ee8ac2c38da59831318346
Daniele Muffato <danixgithub1@gmail.com>
2024-12-05 03:08:35 +0100

    zoxide removed modifications

--- backup/.bashrc.d/init/zoxide.sh
+++ backup/.bashrc.d/init/zoxide.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-eval "$(zoxide init --no-cmd bash)"
-
-alias z='__zoxide_euristically__'
-alias zi='__zoxide_zi'
-
-function __zoxide_euristically__() {
-    \cd "$@" &>/dev/null || __zoxide_z "$@" &>/dev/null
-}
+eval "$(zoxide init bash)"
