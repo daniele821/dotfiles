93786686152e8499dd8bd03989d6f7b39d00f7b4
Daniele Muffato <danixgithub1@gmail.com>
2024-12-17 11:56:53 +0100

    added check if manual installation (02_* run script) fail

--- run/02_updates.sh
+++ run/02_updates.sh
@@ -6,11 +6,19 @@ function ask_user() {
     read -r answer </dev/tty
     [[ "${answer,,}" == 'y' ]]
 }
+function report_fail() {
+    echo -e "\x1b[1;31m${*}\x1b[m"
+    echo -n "type enter to continue... "
+    read -r _ </dev/tty
+}
 
 {
     # download starship
     if ask_user 'Do you really want to download starship locally'; then
         curl -sS https://starship.rs/install.sh | sh
+
+        # check installatin worked
+        [[ -f /usr/local/bin/starship ]] || report_fail 'installation of starship failed!'
     fi
 
     # download fira code nerd font
@@ -22,6 +30,9 @@ function ask_user() {
         wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip -O Firacode.zip
         unzip ./Firacode.zip
         mv ./*.ttf "${FIRACODE_DIR}/"
+
+        # check installation worked
+        rmdir "${FIRACODE_DIR}" &>/dev/null && report_fail 'installation of nerd fonts failed!'
     fi
 
 } </dev/tty
