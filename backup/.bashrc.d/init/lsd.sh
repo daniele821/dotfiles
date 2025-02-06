caaaae755f4184c059764666132ce795db1097a2
Daniele Muffato <danixgithub1@gmail.com>
2025-02-06 05:34:06 +0100

    improved tree alias

--- backup/.bashrc.d/init/lsd.sh
+++ backup/.bashrc.d/init/lsd.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-alias ls='lsd --group-dirs first'
-function tree() {
-    if timeout 0.2 lsd --group-dirs first --tree "$@" &>/dev/null; then
+function __tree__() {
+    timeout 0.2 lsd --group-dirs first --tree "$@" &>/dev/null
+    if [[ $? -ne "124" ]]; then
         lsd --group-dirs first --tree "$@"
     else
         /usr/bin/tree "$@"
-    fi
+    fi 2>/dev/null
 }
+
+alias ls='lsd --group-dirs first'
+alias tree='__tree__'
