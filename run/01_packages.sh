bfd5f54d36ac2a81b028f9cebbe2ddedbed61ea1
Daniele Muffato <danixgithub1@gmail.com>
2025-02-25 16:07:49 +0100

    added basic packages to run these scripts

--- run/01_packages.sh
+++ run/01_packages.sh
@@ -1,7 +1,19 @@
 #!/bin/env bash
 
 {
-    echo 'todo: packages'
+    # required these scripts
+    sudo apt install curl gh -y
+
+    # install neovim latest
+    sudo add-apt-repository ppa:neovim-ppa/unstable -y
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt install neovim -y
+
+    # todo:
+    # sudo apt install zoxide direnv entr bat ripgrep lsd tldr htop
+    #
+    # flatpak
 } </dev/tty
 
 exit 0
