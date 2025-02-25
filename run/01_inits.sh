64a7a8c0e3ec9ff2d9cd9aed6b1c6586a891fab4
Daniele Muffato <danixgithub1@gmail.com>
2025-02-25 18:02:11 +0100

    disable snaps

--- run/01_inits.sh
+++ run/01_inits.sh
@@ -2,3 +2,13 @@
 
 echo "disabling screen locking and sleeping!"
 kde-inhibit --power sleep infinity &
+
+echo "purging snaps out of existance!"
+echo "Package: snapd
+Pin: release a=*
+Pin-Priority: -10" | sudo tee /etc/apt/preferences.d/no-snap.pref
+echo '
+Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozillateam-firefox.pref
