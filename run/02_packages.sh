5f450ff8252fbadffa52e2c4446e6bd009350d9b
Daniele Muffato <danixgithub1@gmail.com>
2025-02-25 18:14:36 +0100

    wip: fixing package installed/removed

--- run/02_packages.sh
+++ run/02_packages.sh
@@ -2,12 +2,13 @@
 
 {
     # purge bloat
-    sudo apt purge --auto-remove -y
+    sudo apt purge --auto-remove plasma-discover* fcitx5* info -y # GUI apps
 
     # add goodies
     sudo apt install curl gh -y                                           # requires to run these scripts
     sudo apt install tree bat zoxide entr direnv ripgrep lsd tldr htop -y # cli tools
     sudo apt install build-essential golang nodejs npm python3.12-venv -y # for programming
+    sudo apt install okular libreoffice-{calc,writer,l10n-it} -y          # GUI apps
 
     # install various ppa & cleanup
     sudo add-apt-repository ppa:neovim-ppa/unstable -y
