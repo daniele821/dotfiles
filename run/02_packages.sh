d73d8ef5ffcd9cb69492727348bc7257f95fa643
Daniele Muffato <danixgithub1@gmail.com>
2025-02-26 13:17:32 +0100

    updated package list

--- run/02_packages.sh
+++ run/02_packages.sh
@@ -2,17 +2,17 @@
 
 {
     # purge bloat
-    sudo apt purge --auto-remove plasma-discover* fcitx5* info -y # GUI apps
-    sudo apt purge --auto-remove khelpcenter kcharselect -y       # GUI apps
-    sudo apt purge --auto-remove apport im-config ksystemlog -y   # GUI apps
+    sudo apt purge --auto-remove fcitx5* info khelpcenter kcharselect -y # GUI apps
+    sudo apt purge --auto-remove apport im-config ksystemlog -y          # GUI apps
+    sudo apt purge --auto-remove plasma-discover-backend-snap -y         # software managers
 
     # add goodies
-    sudo apt install curl gh -y                                           # requires to run these scripts
-    sudo apt install tree bat zoxide entr direnv ripgrep -y               # cli tools
-    sudo apt install lsd tldr htop wl-clipboard fzf -y                    # cli tools
-    sudo apt install build-essential golang nodejs npm python3.12-venv -y # for programming
-    sudo apt install okular libreoffice-{calc,writer,l10n-it} -y          # GUI apps
-    sudo apt install mpv mpv-mpris plasma-discover -y                     # GUI apps
+    sudo apt install curl gh -y                                                 # requires to run these scripts
+    sudo apt install tree bat zoxide entr direnv ripgrep -y                     # cli tools
+    sudo apt install lsd tldr htop wl-clipboard fzf -y                          # cli tools
+    sudo apt install build-essential golang nodejs npm python3.12-venv -y       # for programming
+    sudo apt install okular libreoffice-{calc,writer,l10n-it} mpv mpv-mpris -y  # GUI apps
+    sudo apt install plasma-discover-backend-flatpak plasma-discover flatpak -y # software managers
 
     # install various ppa & cleanup
     sudo add-apt-repository ppa:neovim-ppa/unstable -y
