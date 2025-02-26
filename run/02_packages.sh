92c7887f7a99a36d59d859bd1cae14ef99f59b98
Daniele Muffato <danixgithub1@gmail.com>
2025-02-26 13:44:13 +0100

    removed discover, but added flatpak + flathub

--- run/02_packages.sh
+++ run/02_packages.sh
@@ -4,7 +4,7 @@
     # purge bloat
     sudo apt purge --auto-remove fcitx5* info khelpcenter kcharselect -y # GUI apps
     sudo apt purge --auto-remove apport im-config ksystemlog -y          # GUI apps
-    sudo apt purge --auto-remove plasma-discover-backend-snap -y         # software managers
+    sudo apt purge --auto-remove plasma-discover* -y                     # software managers
 
     # add goodies
     sudo apt install curl gh -y                                                # requires to run these scripts
@@ -12,6 +12,10 @@
     sudo apt install lsd tldr htop wl-clipboard fzf -y                         # cli tools
     sudo apt install build-essential golang nodejs npm python3.12-venv -y      # for programming
     sudo apt install okular libreoffice-{calc,writer,l10n-it} mpv mpv-mpris -y # GUI apps
+    sudo apt install flatpak -y                                                # software managers
+
+    # add flathub to flatpak
+    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
     # install various ppa & cleanup
     sudo add-apt-repository ppa:neovim-ppa/unstable -y
