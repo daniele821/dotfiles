90d80b406f4c47208d8b44471ad04f44d32b4761
Daniele Muffato <danixgithub1@gmail.com>
2025-03-02 22:46:30 +0100

    added back discover

--- run/02_packages.sh
+++ run/02_packages.sh
@@ -4,7 +4,7 @@
     # purge bloat
     sudo apt purge --auto-remove fcitx5* info khelpcenter kcharselect -y # GUI apps
     sudo apt purge --auto-remove apport im-config ksystemlog kate -y     # GUI apps
-    sudo apt purge --auto-remove plasma-discover* -y                     # software managers
+    sudo apt purge --auto-remove plasma-discover-backend-snap -y         # software managers
 
     # add goodies
     sudo apt install curl gh -y                                                # requires to run these scripts
@@ -12,7 +12,7 @@
     sudo apt install lsd tldr htop wl-clipboard fzf -y                         # cli tools
     sudo apt install build-essential golang nodejs npm python3.12-venv -y      # for programming
     sudo apt install okular libreoffice-{calc,writer,l10n-it} mpv mpv-mpris -y # GUI apps
-    sudo apt install flatpak -y                                                # software managers
+    sudo apt install flatpak plasma-discover-backend-flatpak -y                # software managers
 
     # add flathub to flatpak + various flatpak installs
     sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
