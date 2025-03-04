ef85044a039fef8687f65454f7d2f62722f590c5
Daniele Muffato <danixgithub1@gmail.com>
2025-03-05 00:19:41 +0100

    more hw accel packages

--- run/02_packages.sh
+++ run/02_packages.sh
@@ -23,7 +23,8 @@
     sudo dnf --assumeyes install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
     sudo dnf --assumeyes install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
     sudo dnf --assumeyes install ffmpeg --allowerasing
-    sudo dnf --assumeyes install libva-intel-driver intel-media-driver libavcodec-freeworld mesa-va-drivers-freeworld
+    sudo dnf --assumeyes install libva-intel-driver intel-media-driver                                       # intel
+    sudo dnf --assumeyes install libavcodec-freeworld mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld # amd
     sudo dnf --assumeyes install mpv-mpris
 
     # additional copr
