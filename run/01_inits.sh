a3b517f9ec9353820e0b8628b056303b300d8ba8
Daniele Muffato <danixgithub1@gmail.com>
2025-04-19 15:29:53 +0200

    removed 01_inits (very dirty operations)

--- run/01_inits.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-echo "disabling screen locking and sleeping!"
-kde-inhibit --power sleep infinity &
-
-echo "keep refreshing sudo password (until reboot)!"
-sudo -v
-while sleep 1; do sudo -v; done &>/dev/null &
