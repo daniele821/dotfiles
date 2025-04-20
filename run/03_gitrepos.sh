de7a34299e98daa701b4acbd8c01076afa793172
Daniele Muffato <danixgithub1@gmail.com>
2025-04-20 10:46:30 +0200

    updated hardcoded git repo downloader

--- run/03_gitrepos.sh
+++ run/03_gitrepos.sh
@@ -12,6 +12,12 @@ GIT_URL=(
     "git@daniele821.github.com:daniele821/rust.git"
     "git@daniele821.github.com:daniele821/track-payments-data.git"
     "git@daniele821.github.com:daniele821/track-payments"
+    "git@danix1234.github.com:danix1234/unibo-2a.git"
+    "git@danix1234.github.com:danix1234/unibo-2b.git"
+    "git@danix1234.github.com:danix1234/unibo-3a.git"
+    "git@danix1234.github.com:danix1234/unibo-3b.git"
+    "git@danix1234.github.com:danix1234/unibo.git"
+
 )
 GIT_REPO=(
     "/personal/repos/daniele821/dotfiles"
@@ -23,13 +29,40 @@ GIT_REPO=(
     "/personal/repos/daniele821/rust"
     "/personal/repos/daniele821/track-payments-data"
     "/personal/repos/daniele821/track-payments"
+    "/personal/repos/danix1234/unibo-2a"
+    "/personal/repos/danix1234/unibo-2b"
+    "/personal/repos/danix1234/unibo-3a"
+    "/personal/repos/danix1234/unibo-3b"
+    "/personal/repos/danix1234/unibo"
+)
+GIT_EMAIL=(
+    "danixgithub1@gmail.com"
+    "danixgithub1@gmail.com"
+    "danixgithub1@gmail.com"
+    "danixgithub1@gmail.com"
+    "danixgithub1@gmail.com"
+    "danixgithub1@gmail.com"
+    "danixgithub1@gmail.com"
+    "danixgithub1@gmail.com"
+    "danixgithub1@gmail.com"
+    "daniele.muffato@studio.unibo.it"
+    "daniele.muffato@studio.unibo.it"
+    "daniele.muffato@studio.unibo.it"
+    "daniele.muffato@studio.unibo.it"
+    "daniele.muffato@studio.unibo.it"
 )
 
 for ((i = 0; i < "${#GIT_REPO[@]}"; i++)); do
     git_repo="${GIT_REPO[$i]}"
     git_url="${GIT_URL[$i]}"
+    git_email="${GIT_EMAIL[$i]}"
     if [[ ! -d "$git_repo" ]]; then
-        echo -e "cloning \e[1;32m$git_url\e[m into \e[1;33m$git_repo\e[m"
-        git clone "$git_url" "$git_repo"
+        # download git repo
+        echo -e "cloning \e[32m$git_url\e[m into \e[33m$git_repo\e[m"
+        git clone --recurse-submodules "$git_url" "$git_repo"
+
+        # set email
+        echo -e "setting user email to \e[31m$git_email\e[m"
+        git -C "$git_repo" config user.email "$git_email"
     fi
 done
