8cafa5b4089bf7250aadc0babf1daf4be447d604
Daniele Muffato <danixgithub1@gmail.com>
2025-04-28 03:02:52 +0200

    improved ask_user function

--- run/02_localop.sh
+++ run/02_localop.sh
@@ -6,7 +6,15 @@ set -e
 function ask_user() {
     echo -en "\x1b[1;37m${*}? \x1b[m"
     read -r answer </dev/tty
-    [[ "${answer,,}" == 'y' ]]
+    if [[ "${answer,,}" == 'y' ]]; then
+        return 0
+    elif [[ "${answer,,}" == 'n' || "$answer" == '' ]]; then
+        return 1
+    else
+        echo 'invalid answer, retry:'
+        ask_user "$@"
+        return
+    fi
 }
 
 {
