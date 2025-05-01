c9c242b97f00a0bfe3f552526b3f88e7480a3089
Daniele Muffato <danixgithub1@gmail.com>
2025-05-02 01:07:45 +0200

    run dnf operations in a single sudo command

--- run/01_packages.sh
+++ run/01_packages.sh
@@ -2,39 +2,40 @@
 
 set -e
 
+sudo sh -c '
 {
     # upgrade everything
-    sudo dnf --assumeyes upgrade
+    dnf --assumeyes upgrade
 
     # remove bloat
-    sudo dnf --assumeyes remove kpat kmines kmahjongg
-    sudo dnf --assumeyes remove dragon elisa-player neochat
-    sudo dnf --assumeyes remove mediawriter akregator khelpcenter
-    sudo dnf --assumeyes remove kdebugsettings plasma-welcome kde-connect
-    sudo dnf --assumeyes remove kaddressbook kmail kfind
-    sudo dnf --assumeyes remove kjournald skanpage plasma-drkonqi
-    sudo dnf --assumeyes remove abrt akonadi-server kmouth
-    sudo dnf --assumeyes remove krdc krfb krdp
-    sudo dnf --assumeyes remove kolourpaint im-chooser kamoso
-    sudo dnf --assumeyes remove kcharselect firewall-config qrca
-    sudo dnf --assumeyes remove setroubleshoot-server hplip* kde-partitionmanager
+    dnf --assumeyes remove kpat kmines kmahjongg
+    dnf --assumeyes remove dragon elisa-player neochat
+    dnf --assumeyes remove mediawriter akregator khelpcenter
+    dnf --assumeyes remove kdebugsettings plasma-welcome kde-connect
+    dnf --assumeyes remove kaddressbook kmail kfind
+    dnf --assumeyes remove kjournald skanpage plasma-drkonqi
+    dnf --assumeyes remove abrt akonadi-server kmouth
+    dnf --assumeyes remove krdc krfb krdp
+    dnf --assumeyes remove kolourpaint im-chooser kamoso
+    dnf --assumeyes remove kcharselect firewall-config qrca
+    dnf --assumeyes remove setroubleshoot-server hplip* kde-partitionmanager
 
     # install needed programs
-    sudo dnf --assumeyes install git gh # NECESSARY for following scripts!!!
-    sudo dnf --assumeyes install mpv docker kitten neovim htop wireshark
-    sudo dnf --assumeyes install zoxide bat ripgrep lsd tldr gcc jq
+    dnf --assumeyes install git gh # NECESSARY for following scripts!!!
+    dnf --assumeyes install mpv docker kitten neovim htop wireshark
+    dnf --assumeyes install zoxide bat ripgrep lsd tldr gcc jq
 
     # enable rpm-fusion and install multimedia codecs
-    sudo dnf --assumeyes install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-    sudo dnf --assumeyes install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-    sudo dnf --assumeyes install ffmpeg --allowerasing
-    sudo dnf --assumeyes install libva-intel-driver intel-media-driver                                       # intel codecs
-    sudo dnf --assumeyes install libavcodec-freeworld mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld # amd codecs
-    sudo dnf --assumeyes install mpv-mpris
+    dnf --assumeyes install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    dnf --assumeyes install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+    dnf --assumeyes install ffmpeg --allowerasing
+    dnf --assumeyes install libva-intel-driver intel-media-driver                                       # intel codecs
+    dnf --assumeyes install libavcodec-freeworld mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld # amd codecs
+    dnf --assumeyes install mpv-mpris
 
     # remove unnecessary packages
-    sudo dnf --assumeyes autoremove
+    dnf --assumeyes autoremove
 
-} </dev/tty
+} </dev/tty '
 
 exit 0
