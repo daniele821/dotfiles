76a65ddb62b088460f81a4129551bc773617b142
Daniele Muffato <danixgithub1@gmail.com>
2025-05-01 16:55:36 +0200

    remove tor browser old istances
    Tor browser istances take up half a GB of space. Too much!
    
    This also fixes the problem of being able to run tor twice by hard
    failing the oldest istance before it is able to even run!

--- others/tor_instance.sh
+++ others/tor_instance.sh
@@ -13,6 +13,7 @@ pgrep -x tor &>/dev/null && echo 'an other istance of tor browser is already run
 # download tor browser
 json_api="$(curl -s https://aus1.torproject.org/torbrowser/update_3/release/download-linux-x86_64.json)"
 echo "$json_api"
+rm -rf /tmp/tor-browser-temporary-instance
 mkdir -p /tmp/tor-browser-temporary-instance
 cd "$(mktemp -d /tmp/tor-browser-temporary-instance/XXXXXXXXXXXXXXXXXX)" || exit
 wget -O tor.tar.xz "$(echo "$json_api" | jq -r ".binary")"
