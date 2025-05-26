7dd403667bd1cdff176e96da77df54310aa116a5
Daniele Muffato <danixgithub1@gmail.com>
2025-05-26 23:35:06 +0200

    removed qnvim

--- backup/.bashrc.d/01_init.sh
+++ backup/.bashrc.d/01_init.sh
@@ -28,8 +28,3 @@ if exist zoxide; then
     export _ZO_EXCLUDE_DIRS="$HOME:$HOME/.local*:$HOME/.cache*:/afs*:/bin*:/boot*:/dev*:/etc*:/lib*:/lib64*:/media*:/mnt*:/opt*:/proc*:/run*:/sbin*:/srv*:/sys*:/tmp*:/usr*:/var*:/"
     eval "$(zoxide init bash)"
 fi
-
-# neovim multi configs aliases
-function qnvim() {
-    NVIM_APPNAME="nvim/config-db" nvim
-}
