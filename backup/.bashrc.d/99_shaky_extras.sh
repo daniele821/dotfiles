2abe2d52aa12cf12b6128c407be1fccb01c1cb37
Daniele Muffato <danixgithub1@gmail.com>
2025-07-10 06:44:54 +0200

    added function to easily build neovim image, to use with ide

--- backup/.bashrc.d/99_shaky_extras.sh
+++ backup/.bashrc.d/99_shaky_extras.sh
@@ -8,3 +8,12 @@ function ide() {
     podman run --rm -it --security-opt label=type:container_runtime_t -v ".:/app/$DIRNAME" -w "/app/$DIRNAME" localhost/neovim
 }
 
+# build neovim image 
+# WARNING: makes assumption about nvim-config repo structure!
+function build_neovim(){
+    TMP_DIR="$(mktemp -d)"
+    git clone --branch complete --depth 1 https://github.com/daniele821/nvim-config "$TMP_DIR"
+    podman build --rm -t neovim "$TMP_DIR/image"
+    podman image prune -f
+    rm -rf "$TMP_DIR"
+}
