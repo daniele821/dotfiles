f4098d9eaf0ac58627af25f340582856d22d7cb3
Daniele Muffato <danixgithub1@gmail.com>
2025-07-13 23:43:49 +0200

    fixed edit function

--- backup/.bashrc.d/99_shaky_extras.sh
+++ backup/.bashrc.d/99_shaky_extras.sh
@@ -27,7 +27,7 @@ function edit() {
         if [[ -d "$1" ]]; then
             podman run --rm -it --security-opt label=type:container_runtime_t -v "$FULLPATH:/host/$PATHNAME" -w "/host/$PATHNAME" localhost/neovim bash -ic 'nvim'
         else
-            podman run --rm -it --security-opt label=type:container_runtime_t -v "$FULLPATH:/host/$PATHNAME" -w "/host/" localhost/neovim bash -ic 'nvim "$@"' _ "$PATHNAME"
+            podman run --rm -it --security-opt label=type:container_runtime_t -v "$FULLPATH:/host/$PATHNAME" -w "/host/" localhost/neovim bash -ic 'nvim "$@"' _ "/host/$PATHNAME"
         fi
         ;;
     *) # mount multiple files at once, in their fullpath, as to easily avoid conflicts
@@ -39,12 +39,12 @@ function edit() {
         FULLPATHS=("${!tmp_arr[@]}")
         [[ "${#FULLPATHS[@]}" -gt "$MULTI_MOUNT_LIMIT" ]] && echo "cannot mount more then $MULTI_MOUNT_LIMIT files at once" && return 1
         MOUNTS=()
-        ARGS=""
+        ARGS=()
         for arg in "${FULLPATHS[@]}"; do
             ! [[ -e "$arg" ]] && echo "'$arg' does not exists" && return 1
             [[ -d "$arg" ]] && echo "directories not supported in multi-mount mode" && return 1
             MOUNTS+=(-v "$arg:/host/$arg")
-            ARGS+="$("/host/$arg") "
+            ARGS+=("/host$arg")
         done
         podman run --rm -it --security-opt label=type:container_runtime_t "${MOUNTS[@]}" -w /host localhost/neovim bash -ic 'nvim "$@"' _ "${ARGS[@]}"
         ;;
