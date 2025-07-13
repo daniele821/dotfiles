7838981481d9dbbaa88ffc12968177b4bd42e93f
Daniele Muffato <danixgithub1@gmail.com>
2025-07-13 23:38:08 +0200

    ide -> edit, plus now it automatically enters neovim

--- backup/.bashrc.d/99_shaky_extras.sh
+++ backup/.bashrc.d/99_shaky_extras.sh
@@ -10,7 +10,7 @@ function build_neovim() {
 }
 
 # run neovim dev container in current directory
-function ide() {
+function edit() {
     if ! podman image exists localhost/neovim; then
         build_neovim
     fi
@@ -18,15 +18,17 @@ function ide() {
     0) # mount NOTHING
         DIRNAME="$(basename "$(realpath .)")"
         [[ "$DIRNAME" == "/" ]] && DIRNAME="host"
-        podman run --rm -it -w /root localhost/neovim
+        podman run --rm -it -w /root localhost/neovim bash -ic 'nvim'
         ;;
     1) # mount file or directory, and set workdir to that mount location
         FULLPATH="$(realpath -- "$1")"
         ! [[ -e "$FULLPATH" ]] && echo "'$1' does not exists" && return 1
         PATHNAME="$(basename "$FULLPATH")"
-        WORKDIR=""
-        [[ -d "$1" ]] && WORKDIR="$PATHNAME"
-        podman run --rm -it --security-opt label=type:container_runtime_t -v "$FULLPATH:/host/$PATHNAME" -w "/host/$WORKDIR" localhost/neovim
+        if [[ -d "$1" ]]; then
+            podman run --rm -it --security-opt label=type:container_runtime_t -v "$FULLPATH:/host/$PATHNAME" -w "/host/$PATHNAME" localhost/neovim bash -ic 'nvim'
+        else
+            podman run --rm -it --security-opt label=type:container_runtime_t -v "$FULLPATH:/host/$PATHNAME" -w "/host/" localhost/neovim bash -ic 'nvim "$@"' _ "$PATHNAME"
+        fi
         ;;
     *) # mount multiple files at once, in their fullpath, as to easily avoid conflicts
         MULTI_MOUNT_LIMIT=25
@@ -37,12 +39,14 @@ function ide() {
         FULLPATHS=("${!tmp_arr[@]}")
         [[ "${#FULLPATHS[@]}" -gt "$MULTI_MOUNT_LIMIT" ]] && echo "cannot mount more then $MULTI_MOUNT_LIMIT files at once" && return 1
         MOUNTS=()
+        ARGS=""
         for arg in "${FULLPATHS[@]}"; do
             ! [[ -e "$arg" ]] && echo "'$arg' does not exists" && return 1
             [[ -d "$arg" ]] && echo "directories not supported in multi-mount mode" && return 1
             MOUNTS+=(-v "$arg:/host/$arg")
+            ARGS+="$("/host/$arg") "
         done
-        podman run --rm -it --security-opt label=type:container_runtime_t "${MOUNTS[@]}" -w /host localhost/neovim
+        podman run --rm -it --security-opt label=type:container_runtime_t "${MOUNTS[@]}" -w /host localhost/neovim bash -ic 'nvim "$@"' _ "${ARGS[@]}"
         ;;
     esac
 }
