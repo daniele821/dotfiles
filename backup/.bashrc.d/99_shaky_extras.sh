514a7d5880e8a6aeb56eb99f3cfd67efe65be4aa
Daniele Muffato <danixgithub1@gmail.com>
2025-07-15 19:21:26 +0200

    stricter checks on allowed arguments to edit function (disallow not owned files, symlinks and missing files)

--- backup/.bashrc.d/99_shaky_extras.sh
+++ backup/.bashrc.d/99_shaky_extras.sh
@@ -2,13 +2,19 @@
 
 # run neovim dev container in current directory
 function edit() {
+    function fix_file() {
+        FULLPATH="$(realpath -- "$1")"
+        ! [[ -e "$FULLPATH" ]] && echo "'$1' does not exists" >&2 && return 1
+        ! [[ -O "$FULLPATH" ]] && echo "'$1' is not owned by current user" >&2 && return 1
+        [[ -L "${1%/}" ]] && echo "'$1' is a symlink" >&2 && return 1
+        echo "$FULLPATH" && return 0
+    }
     case "$#" in
     0) # mount NOTHING
         podman run --rm -it -w /root ghcr.io/daniele821/neovim bash -ic 'nvim'
         ;;
     1) # mount file or directory, and set workdir to that mount location
-        FULLPATH="$(realpath -- "$1")"
-        ! [[ -e "$FULLPATH" ]] && echo "'$1' does not exists" && return 1
+        FULLPATH="$(fix_file "$1")" || return 1
         DIRNAME="$(dirname "$FULLPATH")"
         if [[ -d "$1" ]]; then
             podman run --rm -it --security-opt label=type:container_runtime_t -v "$FULLPATH:/host/$FULLPATH" -w "/host/$FULLPATH" ghcr.io/daniele821/neovim bash -ic 'nvim'
@@ -19,16 +25,15 @@ function edit() {
     *) # mount multiple files at once, in their fullpath, as to easily avoid conflicts
         declare -A tmp_arr
         for arg in "$@"; do
-            tmp_arr["$(realpath -- "$arg")"]=1
+            path="$(fix_file "$arg")" || return 1
+            tmp_arr["$path"]=1
         done
         FULLPATHS=("${!tmp_arr[@]}")
         MOUNTS=()
         ARGS=()
         for arg in "${FULLPATHS[@]}"; do
-            ! [[ -e "$arg" ]] && echo "'$arg' does not exists" && return 1
-            [[ -d "$arg" ]] && echo "directories not supported in multi-mount mode" && return 1
-            MOUNTS+=(-v "$arg:/host/$arg")
-            ARGS+=("/host$arg")
+            MOUNTS+=(-v "$arg:/host$arg")
+            [[ -f "$arg" ]] && ARGS+=("/host$arg")
         done
         podman run --rm -it --security-opt label=type:container_runtime_t "${MOUNTS[@]}" -w /host ghcr.io/daniele821/neovim bash -ic 'nvim "$@"' _ "${ARGS[@]}"
         ;;
