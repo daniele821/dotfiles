fa53c2fa635e8797f365111ac87ca202d94ca586
Daniele Muffato <danixgithub1@gmail.com>
2025-07-16 18:32:28 +0200

    fixed to eddit func

--- backup/.bashrc.d/99_shaky_extras.sh
+++ backup/.bashrc.d/99_shaky_extras.sh
@@ -39,6 +39,7 @@ function edit() {
         [[ -z "$WORKDIR" ]] && WORKDIR="$(dirname "${ARGS[0]}")"
         ;;
     esac
+    [[ "${#ARGS[@]}" == 0 ]] && ARGS=("$WORKDIR")
 
-    podman run --rm -it --read-only -e "$TZVAR" --security-opt "$SECOP" -v "neovim-state:/root" -v "/:/host" -w "$WORKDIR" "$IMAGE" bash -ilc 'nvim "$@"' _ "${ARGS[@]}"
+    podman run --rm -it -e "$TZVAR" --security-opt "$SECOP" -v "/:/host" -v "neovim-state:/root" -w "$WORKDIR" "$IMAGE" bash -ilc 'nvim "$@"' _ "${ARGS[@]}"
 }
