9697cbd5237910a55d200906b5b3b5af2da9d3b9
Daniele Muffato <danixgithub1@gmail.com>
2025-07-16 10:57:19 +0200

    added multi mount mode for edit

--- backup/.bashrc.d/99_shaky_extras.sh
+++ backup/.bashrc.d/99_shaky_extras.sh
@@ -5,7 +5,7 @@ function edit() {
     function fix_file() {
         local -r FULLPATH="$(realpath -- "$1")"
         case "$FULLPATH" in
-        /tmp | /var/tmp) ;; # exceptions
+        /tmp) ;; # exceptions
         *)
             ! [[ -e "$FULLPATH" ]] && echo "'$1' does not exists" >&2 && return 1
             ! [[ -O "$FULLPATH" ]] && echo "'$1' is not owned by current user" >&2 && return 1
@@ -33,20 +33,26 @@ function edit() {
         fi
         ;;
     *) # mount multiple files at once, in their fullpath, as to easily avoid conflicts
-        echo "TODO" && return 1
-        # declare -A tmp_arr
-        # for arg in "$@"; do
-        #     path="$(fix_file "$arg")" || return 1
-        #     tmp_arr["$path"]=1
-        # done
-        # FULLPATHS=("${!tmp_arr[@]}")
-        # MOUNTS=()
-        # ARGS=()
-        # for arg in "${FULLPATHS[@]}"; do
-        #     MOUNTS+=(-v "$arg:/host$arg")
-        #     [[ -f "$arg" ]] && ARGS+=("/host$arg")
-        # done
-        # podman run --rm -it -e "$TZVAR" --security-opt label=type:container_runtime_t "${MOUNTS[@]}" -w /host "$IMAGE" bash -ic 'nvim "$@"' _ "${ARGS[@]}"
+        declare -A tmp_files
+        declare -A tmp_dirs
+        for arg in "$@"; do
+            path="$(fix_file "$arg")" || return 1
+            if [[ -d "$path" ]]; then
+                tmp_dirs["$path"]=1
+            else
+                dir="$(fix_file "$(dirname "$arg")")" || return 1
+                tmp_dirs["$dir"]=1
+                tmp_files["$path"]=1
+            fi
+        done
+        ALL_FILES=("${!tmp_files[@]}")
+        ALL_DIRS=("${!tmp_dirs[@]}")
+        MOUNTS=()
+        ARGS=()
+        for arg in "${ALL_DIRS[@]}"; do MOUNTS+=(-v "$arg:/host$arg"); done
+        for arg in "${ALL_FILES[@]}"; do ARGS+=("/host$arg"); done
+        [[ "${#ARGS[@]}" == 0 ]] && ARGS+=("/host${ALL_DIRS[0]}")
+        podman run --rm -it -e "$TZVAR" --security-opt label=type:container_runtime_t "${MOUNTS[@]}" -w /host "$IMAGE" bash -ic 'nvim "$@"' _ "${ARGS[@]}"
         ;;
     esac
 }
