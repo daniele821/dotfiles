e86764fdcfe2f81af42a7acff6a657a651b5900b
Daniele Muffato <danixgithub1@gmail.com>
2025-07-17 23:49:54 +0200

    imrpvo edit function (to allow mounting in different place)

--- backup/.bashrc.d/99_shaky_extras.sh
+++ backup/.bashrc.d/99_shaky_extras.sh
@@ -9,14 +9,22 @@ function edit() {
     local ARGS=()
     local arg=""
 
+    function valid_path() {
+        [[ "$(realpath -- "$1")/" != /personal/repos/* ]] && echo "'$1' is not an allowed path" && return 1
+        return 0
+    }
+
     case "$#" in
     0 | 1)
         FULLPATH="$(realpath -- "${1:-.}")"
+        DIRNAME="$(dirname -- "$FULLPATH")"
+        valid_path "$FULLPATH" || return 1
         [[ ! -e "$FULLPATH" ]] && echo "'$1' is not a valid path" && return 1
         if [[ -d "$FULLPATH" ]]; then
             WORKDIR="$FULLPATH"
         elif [[ -f "$FULLPATH" ]]; then
-            WORKDIR="$(dirname "$FULLPATH")"
+            valid_path "$DIRNAME" || return 1
+            WORKDIR="$DIRNAME"
             ARGS=("$FULLPATH")
         else
             echo 'UNREACHABLE (1)' && return 1
@@ -25,6 +33,7 @@ function edit() {
     *)
         for arg in "$@"; do
             FULLPATH="$(realpath -- "${arg}")"
+            valid_path "$FULLPATH"
             [[ ! -e "$FULLPATH" ]] && echo "'$arg' is not a valid path" && return 1
             if [[ -d "$FULLPATH" ]]; then
                 [[ -n "$WORKDIR" ]] && echo "'$arg' is the second directory found" && return 1
@@ -35,19 +44,10 @@ function edit() {
                 echo 'UNREACHABLE (2)' && return 1
             fi
         done
-        [[ -z "$WORKDIR" ]] && WORKDIR="$(dirname "${ARGS[0]}")"
+        [[ -z "$WORKDIR" ]] && WORKDIR="$(dirname -- "${ARGS[0]}")" && ! valid_path "$WORKDIR" && return 1
         ;;
     esac
     [[ "${#ARGS[@]}" == 0 ]] && ARGS=("$WORKDIR")
-
-    function valid_path() {
-        case "$1" in
-        /personal/repos/* | /personal/repos) ;;
-        *) echo "'$1' is not an allowed path" && return 1 ;;
-        esac
-    }
-    valid_path "$WORKDIR" || return 1
-    for arg in "${ARGS[@]}"; do valid_path "$arg"; done || return 1
 
     podman run --rm -it -e "$TZVAR" -v "/personal/repos:/personal/repos:z" -w "$WORKDIR" "$IMAGE" bash -ilc 'nvim "$@"' _ "${ARGS[@]}"
 }
