2749b96262cc34b812889d8669fbedffff28aa9e
Daniele Muffato <danixgithub1@gmail.com>
2025-07-30 17:41:29 +0200

    improved how output for various user init operations are done

--- run/03_user_init.sh
+++ run/03_user_init.sh
@@ -4,9 +4,11 @@ set -e
 
 {
     # global variables often reused
-    FIREFOX_INIT="$HOME/.mozilla/firefox/.init_firefox_profile"
-    FIRACODE_DIR="$HOME/.local/share/fonts/FiraCode"
-    AUTOSAVER_INIT="$HOME/.local/share/.init_autosaver_restorer"
+    INIT_DIR="$HOME/.local/share/init_operations_done"
+    FIREFOX_INIT="$INIT_DIR/firefox-profile"
+    DOTFILES_INIT="$INIT_DIR/dotfiles-restored"
+    FIRACODE_INIT="$INIT_DIR/firacode-installed"
+    DOTFILES_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../)"
 
     # restore firefox backup
     if ! [[ -f "$FIREFOX_INIT" ]]; then
@@ -16,36 +18,35 @@ set -e
         firefox --headless --no-remote --safe-mode about:blank &>/dev/null &
         sleep 2 && kill $!
         find ~/.mozilla/firefox -maxdepth 1 -name '*.default-release' | while read -r profile; do
-            echo "initializing '$profile'..."
             rm -rf "$profile"
-            cp -r "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../others/firefox-init/" "$profile"
+            cp -r "$DOTFILES_ROOT/others/firefox-init/" "$profile"
             mkdir -p "$profile/extensions/"
             cd "$profile/extensions/"
-            echo "downloading darkreader extension..."
             curl -s -L "https://addons.mozilla.org/firefox/downloads/latest/darkreader/addon-953454-latest.xpi" -o addon@darkreader.org.xpi
-            echo "downloading sponsorblock extension..."
             curl -s -L "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/addon-406847-latest.xpi" -o sponsorBlocker@ajay.app.xpi
-            echo "downloading ublock origin extension..."
             curl -s -L "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-607454-latest.xpi" -o uBlock0@raymondhill.net.xpi
         done
-        touch "$FIREFOX_INIT"
+        mkdir -p "$INIT_DIR" && touch "$FIREFOX_INIT"
     fi
 
     # install Firacode font
-    if ! [[ -d "$FIRACODE_DIR" ]]; then
+    if ! [[ -f "$FIRACODE_INIT" ]]; then
+        FIRACODE_DIR="$HOME/.local/share/fonts/FiraCode"
+        rm -rf "$FIRACODE_DIR"
         mkdir -p "$FIRACODE_DIR"
         cd "$FIRACODE_DIR"
-        wget -O Firacode.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
-        unzip Firacode.zip
+        echo "installing firacode font..."
+        curl -s -L -o Firacode.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
+        unzip Firacode.zip >/dev/null
         rm Firacode.zip LICENSE README.md
+        mkdir -p "$INIT_DIR" && touch "$FIRACODE_INIT"
     fi
 
     # restore files and download git repos
-    if ! [[ -f "$AUTOSAVER_INIT" ]]; then
-        SCRIPT_PATH="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../autosaver")"
-        "$SCRIPT_PATH" git
-        "$SCRIPT_PATH" restoreall
-        touch "$AUTOSAVER_INIT"
+    if ! [[ -f "$DOTFILES_INIT" ]]; then
+        echo "restoring all dotfiles"
+        "$DOTFILES_ROOT/autosaver" restoreall >/dev/null
+        mkdir -p "$INIT_DIR" && touch "$DOTFILES_INIT"
     fi
 
 } </dev/tty
