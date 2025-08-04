7934fe373b77556ddd1e516797584526e3ee29c9
Daniele Muffato <danixgithub1@gmail.com>
2025-08-04 19:07:22 +0200

    added meccanism to restart new container if running one cannot be used (edit)

--- backup/.bashrc.d/02_various.sh
+++ backup/.bashrc.d/02_various.sh
@@ -57,9 +57,13 @@ function edit(){
         running) ;;
         paused) echo -e "\e[1;33mcontainer found paused: unpausing it...\e[m"; podman unpause "$BG_CONTAINER" ;;
         exited) echo -e "\e[1;33mcontainer found stopped: launching it...\e[m"; podman start "$BG_CONTAINER" ;;
-        *) echo -e "\e[1;33mcountainer found in state '$state': starting a new one...\e[m"; podman rm -f "$BG_CONTAINER" ;;
+        *) echo -e "\e[1;33mcontainer found in state '$state': starting a new one...\e[m"; podman rm -f "$BG_CONTAINER" ;;
     esac
-    podman exec --detach-keys="" -it -w /data "$BG_CONTAINER" bash -il
+    if ! podman exec --detach-keys="" -it -w /data "$BG_CONTAINER" bash -il; then
+        echo -e "\e[1;33mrunning container cannot be used. starting a new one...\e[m"
+        local BG_CONTAINER="$(podman run --detach-keys "" -v data_neovim:/data -d --init -e "TZ=$(timedatectl show --property=Timezone --value)" "$NEOVIM_IMAGE" sleep infinity)"
+        podman exec --detach-keys="" -it -w /data "$BG_CONTAINER" bash -il
+    fi
 }
 
 unset command_not_found_handle
