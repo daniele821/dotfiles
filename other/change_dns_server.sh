bd9d097800ac9881a64e453f4cc911fea443b2f5
- <->
2025-08-21 14:32:59 +0200

    small fix

--- other/change_dns_server.sh
+++ other/change_dns_server.sh
@@ -13,8 +13,7 @@ case "$answer" in
 DNS=94.140.14.14#dns.adguard-dns.com 94.140.15.15#dns.adguard-dns.com 2a10:50c0::ad1:ff#dns.adguard-dns.com 2a10:50c0::ad2:ff#dns.adguard-dns.com
 DNSOverTLS=yes
 DNSSEC=yes
-Domains=~.
-' | sudo tee /etc/systemd/resolved.conf
+Domains=~.' | sudo tee /etc/systemd/resolved.conf
     ;;
 2) sudo rm /etc/systemd/resolved.conf ;;
 *) exit 0 ;;
