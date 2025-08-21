9608e8c25ca41cb543cea3c5f4d7fef43a47ad87
- <->
2025-08-21 14:38:05 +0200

    no need for #dns.adguard-dns.com

--- other/change_dns_server.sh
+++ other/change_dns_server.sh
@@ -10,7 +10,7 @@ read -rp "Do you want to (1) set or (2) remove adblocking dns? " answer
 case "$answer" in
 1)
     echo '[Resolve]
-DNS=94.140.14.14#dns.adguard-dns.com 94.140.15.15#dns.adguard-dns.com 2a10:50c0::ad1:ff#dns.adguard-dns.com 2a10:50c0::ad2:ff#dns.adguard-dns.com
+DNS=94.140.14.14 94.140.15.15 2a10:50c0::ad1:ff 2a10:50c0::ad2:ff
 DNSOverTLS=yes
 DNSSEC=yes
 Domains=~.' | sudo tee /etc/systemd/resolved.conf
