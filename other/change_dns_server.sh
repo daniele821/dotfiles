54764176feebce8cef7657f88c029b9bf2c5291c
- <->
2025-08-21 14:15:44 +0200

    enable dnssec

--- other/change_dns_server.sh
+++ other/change_dns_server.sh
@@ -12,6 +12,7 @@ case "$answer" in
     echo '[Resolve]
 DNS=94.140.14.14 94.140.15.15 2a10:50c0::ad1:ff 2a10:50c0::ad2:ff
 DNSOverTLS=yes
+DNSSEC=yes
 Domains=~.
 ' | sudo tee /etc/systemd/resolved.conf
     ;;
