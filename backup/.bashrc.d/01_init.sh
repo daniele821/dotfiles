977447641aa50d6ac0b3b7c4a133e5d4091b5151
- <->
2025-09-01 18:42:07 +0200

    zoxide now does not exclude any dir

--- backup/.bashrc.d/01_init.sh
+++ backup/.bashrc.d/01_init.sh
@@ -25,7 +25,6 @@ fi
 
 # zoxide init
 if exist zoxide; then
-    export _ZO_EXCLUDE_DIRS="$HOME:$HOME/.local*:$HOME/.cache*:/afs*:/bin*:/boot*:/dev*:/etc*:/lib*:/lib64*:/media*:/mnt*:/opt*:/proc*:/run*:/sbin*:/srv*:/sys*:/tmp*:/usr*:/var*:/"
     eval "$(zoxide init bash)"
 fi
 
